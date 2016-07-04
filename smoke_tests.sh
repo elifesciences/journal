76b11b0c602604b2478ecc9a279d6416e1e8acb8
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-04 09:48:05 +0100

    Added /status to smoke tests

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -2,3 +2,4 @@
 set -e
 
 [ $(curl --write-out %{http_code} --silent --output /dev/null localhost:1240/favicon.ico) == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null localhost:1240/status) == 200 ]
