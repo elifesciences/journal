fa9cedda55635d843a7c4b0eabd0c755a6f0e101
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-04 11:53:46 +0100

    Smoke tests must run from inside the project machine

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-[ $(curl --write-out %{http_code} --silent --output /dev/null localhost:1240/favicon.ico) == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null localhost:1240/status) == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null localhost/favicon.ico) == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null localhost/status) == 200 ]
