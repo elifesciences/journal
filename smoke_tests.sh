bfd6e3b099bb018041f92a5d71c4dbc7e27530b2
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-04 14:06:02 +0100

    Trying to make smoke tests more verbose

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 [ $(curl --write-out %{http_code} --silent --output /dev/null localhost/favicon.ico) == 200 ]
 [ $(curl --write-out %{http_code} --silent --output /dev/null localhost/status) == 200 ]
