41a48e11b7e12b7bd0f3e2eed75608efab9a6841
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-04 09:47:27 +0100

    Added a /status route

---
+++ src/Controller/StatusController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace eLife\Journal\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class StatusController
+{
+    public function indexAction()
+    {
+        return new Response("");
+    }
+}
