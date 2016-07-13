b16e3b3d04ae71930d436eb73faf708a552df0bb
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-13 08:49:12 +0100

    Changed path of bash

--- project_tests.sh
+++ project_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 vendor/bin/phpunit --log-junit build/phpunit.xml
--- smoke_tests.sh
+++ smoke_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -ex
 
 [ $(curl --write-out %{http_code} --silent --output /dev/null localhost/favicon.ico) == 200 ]
