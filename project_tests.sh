a105e0f24e60cd8f242479f846fd4600d2ad809f
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-07-28 15:45:25 +0100

    Run proofreader on all code (#9)

--- project_tests.sh
+++ project_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-proofreader src/
+proofreader app/ bin/ features/ src/ test/ web/
 vendor/bin/phpunit --log-junit build/phpunit.xml
 vendor/bin/behat --tags '~wip' --format junit --format pretty
 
