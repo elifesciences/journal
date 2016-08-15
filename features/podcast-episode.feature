d4ba70db6d24348d3670aa372d4c3dc24f5110fb
Chris Wilkinson <c.wilkinson@elifesciences.org>
2016-08-15 14:49:06 +0100

    Add podcast episode page

---
+++ features/podcast-episode.feature
@@ -0,0 +1,22 @@
+@podcast
+Feature: Podcast episode
+  In order to...
+  As a...
+  I want...
+
+  Rules:
+  - Clicking a chapter title will start the audio player playing that chapter
+  - Related list shows the first article covered by each of the chapters
+
+  @wip
+  Scenario: Clicking a chapter title plays the chapter
+    Given there is a podcast episode with two chapters
+    When I go the podcast episode page
+    And I click on the second chapter's title
+    Then the audio player should start playing the second chapter
+
+  @wip
+  Scenario: Related list shows first article for each chapter
+    Given there is a podcast episode with two chapters
+    When I go the podcast episode page
+    Then I should see the two articles covered by the chapters
---
+++ test/Controller/PodcastEpisodeControllerTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace test\eLife\Journal\Controller;
+
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+
+final class PodcastEpisodeControllerTest extends PageTestCase
+{
+    /**
+     * @test
+     */
+    public function it_displays_a_podcast_episode_page()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', $this->getUrl());
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertSame('Episode title', $crawler->filter('.content-header__title')->text());
+    }
+
+    /**
+     * @test
+     */
+    public function it_displays_a_404_if_the_episode_is_not_found()
+    {
+        $client = static::createClient();
+
+        static::mockApiResponse(
+            new Request(
+                'GET',
+                'http://api.elifesciences.org/podcast-episodes/2',
+                ['Accept' => 'application/vnd.elife.podcast-episode+json; version=1']
+            ),
+            new Response(
+                404,
+                [
+                    'Content-Type' => 'application/problem+json',
+                ],
+                json_encode([
+                    'title' => 'Not found',
+                ])
+            )
+        );
+
+        $client->request('GET', '/podcast/episode2');
+
+        $this->assertSame(404, $client->getResponse()->getStatusCode());
+    }
+
+    protected function getUrl() : string
+    {
+        $this->mockApiResponse(
+            new Request(
+                'GET',
+                'http://api.elifesciences.org/podcast-episodes/1',
+                ['Accept' => 'application/vnd.elife.podcast-episode+json; version=1']
+            ),
+            new Response(
+                200,
+                ['Content-Type' => 'application/vnd.elife.podcast-episode+json; version=1'],
+                json_encode([
+                    'number' => 1,
+                    'title' => 'Episode title',
+                    'published' => '2010-01-01T00:00:00+00:00',
+                    'image' => [
+                        'alt' => '',
+                        'sizes' => [
+                            '2:1' => [
+                                900 => 'https://placehold.it/900x450',
+                                1800 => 'https://placehold.it/1800x900',
+                            ],
+                            '16:9' => [
+                                250 => 'https://placehold.it/250x141',
+                                500 => 'https://placehold.it/500x281',
+                            ],
+                            '1:1' => [
+                                70 => 'https://placehold.it/70x70',
+                                140 => 'https://placehold.it/140x140',
+                            ],
+                        ],
+                    ],
+                    'impactStatement' => 'Experiment impact statement',
+                    'mp3' => 'https://www.example.com/episode1.mp3',
+                ])
+            )
+        );
+
+        return '/podcast/episode1';
+    }
+}
