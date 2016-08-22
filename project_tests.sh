46b14221f177de251e3340ab5b28021124e93171
Giorgio Sironi <g.sironi@elifesciences.org>
2016-08-22 12:09:29 +0100

    Removal of build files should never fail

--- project_tests.sh
+++ project_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-rm build/*.xml
+rm -f build/*.xml
 proofreader app/ bin/ features/ src/ test/ web/
 vendor/bin/phpunit --log-junit build/phpunit.xml
 vendor/bin/behat --tags '~wip' --format junit --format pretty
