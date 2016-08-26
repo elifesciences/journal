71a3af09949dcb50254ea99ed77ab4e3cd2dba0f
Chris Wilkinson <c.wilkinson@elifesciences.org>
2016-08-26 16:44:27 +0100

    Smoke test Puli

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -2,5 +2,6 @@
 set -ex
 
 [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/favicon.ico) == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/assets/css/all.css) == 200 ]
 [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/status) == 200 ]
 #[ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)) == 200 ]
