3ce046ff5768b8b93bc799bf7476dbfd825a43c9
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-09-14 09:02:09 +0100

    Add article body content (#67)
    * Add article body content
    
    * Tables are ok
    
    * Bypass PHPCPD

---
+++ src/ViewModel/Table.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace eLife\Journal\ViewModel;
+
+use eLife\Patterns\ArrayFromProperties;
+use eLife\Patterns\ReadOnlyArrayAccess;
+use eLife\Patterns\SimplifyAssets;
+use eLife\Patterns\ViewModel;
+
+/**
+ * @SuppressWarnings(ForbiddenAbleSuffix)
+ */
+final class Table implements ViewModel
+{
+    use ArrayFromProperties;
+    use ReadOnlyArrayAccess;
+    use SimplifyAssets;
+
+    private $table;
+    private $footer;
+
+    public function __construct(string $table, string $footer = null)
+    {
+        $this->table = $table;
+        $this->footer = $footer;
+    }
+
+    public function getTemplateName() : string
+    {
+        return '/elife/journal/patterns/table.mustache';
+    }
+}
