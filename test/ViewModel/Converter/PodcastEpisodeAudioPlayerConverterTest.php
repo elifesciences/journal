09b135797b75b1f119c04a70d0ae98a69e5bb5a0
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-09 16:11:10 +0000

    Added PodcastEpisodeAudioPlayerConverterTest

---
+++ test/ViewModel/Converter/PodcastEpisodeAudioPlayerConverterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace test\eLife\Journal\ViewModel\Converter;
+
+use eLife\ApiSdk\Model\PodcastEpisode;
+use eLife\Journal\ViewModel\Converter\PodcastEpisodeAudioPlayerConverter;
+use eLife\Patterns\ViewModel\AudioPlayer;
+use eLife\Patterns\ViewModel\MediaChapterListingItem;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Puli\UrlGenerator\Api\UrlGenerator as PuliUrlGenerator;
+use eLife\Journal\ViewModel\Converter\ViewModelConverter;
+
+final class PodcastEpisodeAudioPlayerConverterTest extends ModelConverterTestCase
+{
+    # multiple models
+    protected $models = ['podcast-episode'];
+    protected $class = PodcastEpisode::class;
+    protected $viewModelClass = AudioPlayer::class;
+    protected $samples = 'complete';
+    //protected $samples = '*'; or delete it
+
+    /**
+     * @before
+     */
+    public function setUpConverter()
+    {
+        $this->viewModelConverter = $this->createMock(ViewModelConverter::class);
+        $this->viewModelConverter->expects($this->any())
+            ->method('convert')
+            ->will($this->returnValue($this->aMediaChapterListingItem()));
+
+        $this->converter = new PodcastEpisodeAudioPlayerConverter($this->viewModelConverter);
+    }
+
+    private function aMediaChapterListingItem()
+    {
+        return new MediaChapterListingItem("1", 0, 1);
+    }
+}
