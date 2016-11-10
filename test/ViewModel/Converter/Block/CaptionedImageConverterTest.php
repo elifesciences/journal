0ffa0d11807490a686d31b858c6ca7e161d52375
Giorgio Sironi <g.sironi@elifesciences.org>
2016-11-10 16:29:30 +0000

    Updating CaptionedImageConverterTest to changes of figure supplements

--- test/ViewModel/Converter/Block/CaptionedImageConverterTest.php
+++ test/ViewModel/Converter/Block/CaptionedImageConverterTest.php
@@ -3,20 +3,27 @@
 namespace test\eLife\Journal\ViewModel\Converter\Block;
 
 use eLife\ApiSdk\Model\Block\Image;
+use eLife\Journal\ViewModel\AssetViewerInlineSet;
 use eLife\Journal\ViewModel\Converter\Block\CaptionedImageConverter;
-use eLife\Patterns\ViewModel\CaptionedAsset;
+use eLife\Journal\ViewModel\Converter\ViewModelConverter;
 
 final class CaptionedImageConverterTest extends BlockConverterTestCase
 {
     protected $class = Image::class;
-    protected $viewModelClass = CaptionedAsset::class;
+    protected $viewModelClass = AssetViewerInlineSet::class;
 
     /**
      * @before
      */
     public function setUpConverter()
     {
-        $this->converter = new CaptionedImageConverter();
+        $this->converter = new CaptionedImageConverter(
+            $viewModelConverter = $this->createMock(ViewModelConverter::class)
+        );
+        $viewModelConverter
+            ->expects($this->any())
+            ->method('convert')
+            ->will($this->returnValue(new AssetViewerInlineSet()));
     }
 
     public function blocks()
