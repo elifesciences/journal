c4c0e0c68df7fbfbff0c060589ac491c96d30cd5
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-11-15 13:19:05 +0000

    Let known vulnerabilities fail builds (#123)

--- project_tests.sh
+++ project_tests.sh
@@ -2,6 +2,7 @@
 set -e
 
 rm -f build/*.xml
+bin/console security:check
 proofreader app/ bin/ features/ src/ test/ web/
 vendor/bin/phpunit --log-junit build/phpunit.xml
 vendor/bin/behat --strict --tags '~wip' --format junit --format pretty
