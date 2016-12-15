de3ad0515d40211c764a9ae2246096df2325cf78
Chris Wilkinson <c.wilkinson@elifesciences.org>
2016-12-15 08:15:40 +0000

    Add subjects list to the homepage

--- test/Controller/HomeControllerTest.php
+++ test/Controller/HomeControllerTest.php
@@ -21,6 +21,64 @@ final class HomeControllerTest extends PageTestCase
         $this->assertContains('No articles available.', $crawler->filter('main')->text());
     }
 
+    /**
+     * @test
+     */
+    public function it_displays_a_subjects_list()
+    {
+        $client = static::createClient();
+
+        $this->mockApiResponse(
+            new Request(
+                'GET',
+                'http://api.elifesciences.org/subjects?page=1&per-page=100&order=asc',
+                ['Accept' => 'application/vnd.elife.subject-list+json; version=1']
+            ),
+            new Response(
+                200,
+                ['Content-Type' => 'application/vnd.elife.subject-list+json; version=1'],
+                json_encode([
+                    'total' => 1,
+                    'items' => [
+                        [
+                            'id' => 'subject',
+                            'name' => 'Subject name',
+                            'impactStatement' => 'Subject impact statement.',
+                            'image' => [
+                                'banner' => [
+                                    'alt' => '',
+                                    'sizes' => [
+                                        '2:1' => [
+                                            900 => 'https://placehold.it/900x450',
+                                            1800 => 'https://placehold.it/1800x900',
+                                        ],
+                                    ],
+                                ],
+                                'thumbnail' => [
+                                    'alt' => '',
+                                    'sizes' => [
+                                        '16:9' => [
+                                            250 => 'https://placehold.it/250x141',
+                                            500 => 'https://placehold.it/500x281',
+                                        ],
+                                        '1:1' => [
+                                            70 => 'https://placehold.it/70x70',
+                                            140 => 'https://placehold.it/140x140',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ])
+            )
+        );
+
+        $crawler = $client->request('GET', $this->getUrl());
+
+        $this->assertSame('Subject name', trim($crawler->filter('.site-links-list__list_item')->text()));
+    }
+
     protected function getUrl() : string
     {
         $this->mockApiResponse(
