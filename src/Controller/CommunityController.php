18eecd7fc1914bca7a0e6957786c4df00e94c72d
Chris Wilkinson <c.wilkinson@elifesciences.org>
2016-12-20 10:17:00 +0000

    Add banner images

--- src/Controller/CommunityController.php
+++ src/Controller/CommunityController.php
@@ -2,6 +2,7 @@
 
 namespace eLife\Journal\Controller;
 
+use eLife\Patterns\ViewModel\BackgroundImage;
 use eLife\Patterns\ViewModel\ContentHeaderNonArticle;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -11,7 +12,11 @@ final class CommunityController extends Controller
     {
         $arguments = $this->defaultPageArguments();
 
-        $arguments['contentHeader'] = ContentHeaderNonArticle::basic('Community');
+        $arguments['contentHeader'] = ContentHeaderNonArticle::basic('Community', true, null, null, null,
+            new BackgroundImage(
+                $this->get('puli.url_generator')->generateUrl('/elife/journal/images/banners/community-lo-res.jpg'),
+                $this->get('puli.url_generator')->generateUrl('/elife/journal/images/banners/community-hi-res.jpg')
+            ));
 
         return new Response($this->get('templating')->render('::community.html.twig', $arguments));
     }
