422c1339d1d49cf05780cf808dcb40c36819cf08
Chris Wilkinson <c.wilkinson@elifesciences.org>
2016-12-21 11:18:03 +0000

    Only ping in CI

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -4,5 +4,9 @@ set -ex
 [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/favicon.ico) == 200 ]
 [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/assets/css/all.css) == 200 ]
 [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/ping) == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/status) == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)) == 200 ]
+
+if [ "$ENVIRONMENT_NAME" != "ci" ]
+  then
+    [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/status) == 200 ]
+    [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)) == 200 ]
+fi
