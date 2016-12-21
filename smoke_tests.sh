c9907ead41ec8e1ec16118f9ea17037df806057a
Chris Wilkinson <chriswilkinson84@gmail.com>
2016-12-21 12:07:40 +0000

    Homepage should now work (#176)
    * Homepage should now work
    
    * Separate pinging from status checking
    
    * Only ping in CI

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -3,5 +3,10 @@ set -ex
 
 [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/favicon.ico) == 200 ]
 [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/assets/css/all.css) == 200 ]
-[ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/status) == 200 ]
-#[ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)) == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/ping) == 200 ]
+
+if [ "$ENVIRONMENT_NAME" != "ci" ]
+  then
+    [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/status) == 200 ]
+    [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)) == 200 ]
+fi
