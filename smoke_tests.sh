4bde448e4aaa289b99f0bdcb27fe02cf664ce315
Chris Wilkinson <c.wilkinson@elifesciences.org>
2016-12-21 10:02:17 +0000

    Homepage should now work

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -4,4 +4,4 @@ set -ex
 [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/favicon.ico) == 200 ]
 [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/assets/css/all.css) == 200 ]
 [ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)/status) == 200 ]
-#[ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)) == 200 ]
+[ $(curl --write-out %{http_code} --silent --output /dev/null $(hostname)) == 200 ]
