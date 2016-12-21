25e5a4afcfe66f1fb035d407a0d81a4df76d238d
Giorgio Sironi <g.sironi@elifesciences.org>
2016-12-21 10:54:24 +0000

    features/ are tests too

--- project_tests.sh
+++ project_tests.sh
@@ -3,8 +3,8 @@ set -e
 
 rm -f build/*.xml
 bin/console security:check
-proofreader app/ bin/ features/ src/ web/
-proofreader --no-phpcpd test/
+proofreader app/ bin/ src/ web/
+proofreader --no-phpcpd features/ test/
 vendor/bin/phpunit --log-junit build/phpunit.xml
 vendor/bin/behat --strict --tags '~wip' --format junit --format pretty
 
