e8de1cd671afcb5ee3adc04e3c080de14b7e594a
David Moulton <d.moulton@elifesciences.org>
2017-01-18 10:22:43 +0000

    Cause /status page to throw uncaught js error
    For use with investigating New Relic's client-side capabilities. REMOVE BEFORE LAUNCH.

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -14,13 +14,20 @@ final class StatusController extends Controller
 
     public function statusAction() : Response
     {
+      // TODO: Remove $jsErrorSetup before launch
+      $jsErrorSetup = '<script>';
+      $jsErrorSetup .= 'console.log(\'This page is used for configuring investigating New Relic JavaScript logging etc.\');' . "\n";
+      $jsErrorSetup .= 'console.log(\'About to deliberately throw an uncaught ReferenceError.\');' . "\n";
+      $jsErrorSetup .= 'throw new ReferenceError(\'Deliberately uncaught. Where have I ended up?\');' . "\n";
+      $jsErrorSetup .= '</script>';
+
         try {
             $this->get('elife.api_sdk.search')[0];
         } catch (Throwable $e) {
-            return $this->createResponse('<html><head><title>Status</title></head><body>Everything is not ok.</body></html>', 500);
+            return $this->createResponse('<html><head><title>Status</title></head><body>Everything is not ok.' . $jsErrorSetup . ' </body></html>', 500);
         }
 
-        return $this->createResponse('<html><head><title>Status</title></head><body>Everything is ok.</body></html>');
+        return $this->createResponse('<html><head><title>Status</title></head><body>Everything is ok.' . $jsErrorSetup . '</body></html>');
     }
 
     private function createResponse(string $body = '', int $statusCode = Response::HTTP_OK, array $headers = [])
