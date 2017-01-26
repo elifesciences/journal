918427b098d89dcafc9a116d8dc901c970d2b13e
Chris Wilkinson <chriswilkinson84@gmail.com>
2017-01-26 08:13:28 +0000

    Add search listing (#218)

--- features/bootstrap/SearchContext.php
+++ features/bootstrap/SearchContext.php
@@ -1,11 +1,226 @@
 <?php
 
 use Behat\Gherkin\Node\TableNode;
+use eLife\ApiSdk\ApiSdk;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
 
 final class SearchContext extends Context
 {
+    private $numberOfArticles;
+
+    /**
+     * @Given /^there are (\d+) articles about \'([^\']*)\'$/
+     */
+    public function thereAreArticlesAbout(int $number, string $keyword)
+    {
+        $this->numberOfArticles = $number;
+
+        $articles = [];
+
+        $today = (new DateTimeImmutable())->setTime(0, 0, 0);
+
+        for ($i = $number; $i > 0; --$i) {
+            $i = str_pad($i, 5, '0', STR_PAD_LEFT);
+            $articles[] = [
+                'status' => 'poa',
+                'stage' => 'published',
+                'id' => "$i",
+                'version' => 1,
+                'type' => 'research-article',
+                'doi' => '10.7554/eLife.'.$i,
+                'title' => 'Article '.$i.' title',
+                'published' => $today->format(ApiSdk::DATE_FORMAT),
+                'versionDate' => $today->format(ApiSdk::DATE_FORMAT),
+                'statusDate' => $today->format(ApiSdk::DATE_FORMAT),
+                'volume' => 5,
+                'elocationId' => 'e'.$i,
+                'copyright' => [
+                    'license' => 'CC-BY-4.0',
+                    'holder' => 'Author et al',
+                    'statement' => 'Creative Commons Attribution License.',
+                ],
+                'authorLine' => 'Foo Bar',
+            ];
+        }
+
+        $this->mockApiResponse(
+            new Request(
+                'GET',
+                'http://api.elifesciences.org/search?for=&page=1&per-page=1&sort=date&order=desc',
+                ['Accept' => 'application/vnd.elife.search+json; version=1']
+            ),
+            new Response(
+                200,
+                ['Content-Type' => 'application/vnd.elife.search+json; version=1'],
+                json_encode([
+                    'total' => $number,
+                    'items' => [],
+                    'subjects' => [
+                        [
+                            'id' => 'subject',
+                            'name' => 'Some subject',
+                            'results' => 0,
+                        ],
+                    ],
+                    'types' => [
+                        'correction' => 0,
+                        'editorial' => 0,
+                        'feature' => 0,
+                        'insight' => 0,
+                        'research-advance' => 0,
+                        'research-article' => 0,
+                        'research-exchange' => 0,
+                        'retraction' => 0,
+                        'registered-report' => 0,
+                        'replication-study' => 0,
+                        'short-report' => 0,
+                        'tools-resources' => 0,
+                        'blog-article' => 0,
+                        'collection' => 0,
+                        'event' => 0,
+                        'interview' => 0,
+                        'labs-experiment' => 0,
+                        'podcast-episode' => 0,
+                    ],
+                ])
+            )
+        );
+
+        $this->mockApiResponse(
+            new Request(
+                'GET',
+                'http://api.elifesciences.org/search?for=&page=1&per-page=6&sort=date&order=desc',
+                ['Accept' => 'application/vnd.elife.search+json; version=1']
+            ),
+            new Response(
+                200,
+                ['Content-Type' => 'application/vnd.elife.search+json; version=1'],
+                json_encode([
+                    'total' => $number,
+                    'items' => [],
+                    'subjects' => [
+                        [
+                            'id' => 'subject',
+                            'name' => 'Some subject',
+                            'results' => 0,
+                        ],
+                    ],
+                    'types' => [
+                        'correction' => 0,
+                        'editorial' => 0,
+                        'feature' => 0,
+                        'insight' => 0,
+                        'research-advance' => 0,
+                        'research-article' => 0,
+                        'research-exchange' => 0,
+                        'retraction' => 0,
+                        'registered-report' => 0,
+                        'replication-study' => 0,
+                        'short-report' => 0,
+                        'tools-resources' => 0,
+                        'blog-article' => 0,
+                        'collection' => 0,
+                        'event' => 0,
+                        'interview' => 0,
+                        'labs-experiment' => 0,
+                        'podcast-episode' => 0,
+                    ],
+                ])
+            )
+        );
+
+        $this->mockApiResponse(
+            new Request(
+                'GET',
+                "http://api.elifesciences.org/search?for=$keyword&page=1&per-page=1&sort=date&order=desc",
+                ['Accept' => 'application/vnd.elife.search+json; version=1']
+            ),
+            new Response(
+                200,
+                ['Content-Type' => 'application/vnd.elife.search+json; version=1'],
+                json_encode([
+                    'total' => $number,
+                    'items' => [$articles[0]],
+                    'subjects' => [
+                        [
+                            'id' => 'subject',
+                            'name' => 'Some subject',
+                            'results' => 0,
+                        ],
+                    ],
+                    'types' => [
+                        'correction' => 0,
+                        'editorial' => 0,
+                        'feature' => 0,
+                        'insight' => 0,
+                        'research-advance' => 0,
+                        'research-article' => $this->numberOfArticles,
+                        'research-exchange' => 0,
+                        'retraction' => 0,
+                        'registered-report' => 0,
+                        'replication-study' => 0,
+                        'short-report' => 0,
+                        'tools-resources' => 0,
+                        'blog-article' => 0,
+                        'collection' => 0,
+                        'event' => 0,
+                        'interview' => 0,
+                        'labs-experiment' => 0,
+                        'podcast-episode' => 0,
+                    ],
+                ])
+            )
+        );
+
+        foreach (array_chunk($articles, 6) as $i => $articleChunk) {
+            $page = $i + 1;
+
+            $this->mockApiResponse(
+                new Request(
+                    'GET',
+                    "http://api.elifesciences.org/search?for=$keyword&page=$page&per-page=6&sort=date&order=desc",
+                    ['Accept' => 'application/vnd.elife.search+json; version=1']
+                ),
+                new Response(
+                    200,
+                    ['Content-Type' => 'application/vnd.elife.search+json; version=1'],
+                    json_encode([
+                        'total' => $number,
+                        'items' => $articleChunk,
+                        'subjects' => [
+                            [
+                                'id' => 'subject',
+                                'name' => 'Some subject',
+                                'results' => 0,
+                            ],
+                        ],
+                        'types' => [
+                            'correction' => 0,
+                            'editorial' => 0,
+                            'feature' => 0,
+                            'insight' => 0,
+                            'research-advance' => 0,
+                            'research-article' => $this->numberOfArticles,
+                            'research-exchange' => 0,
+                            'retraction' => 0,
+                            'registered-report' => 0,
+                            'replication-study' => 0,
+                            'short-report' => 0,
+                            'tools-resources' => 0,
+                            'blog-article' => 0,
+                            'collection' => 0,
+                            'event' => 0,
+                            'interview' => 0,
+                            'labs-experiment' => 0,
+                            'podcast-episode' => 0,
+                        ],
+                    ])
+                )
+            );
+        }
+    }
+
     /**
      * @Given /^I am reading an article:$/
      */
@@ -96,9 +311,12 @@ final class SearchContext extends Context
         $this->visitPath('/content/1/e00001');
     }
 
-    private function createSubjectId(string $subjectName) : string
+    /**
+     * @Given /^I am on the search page$/
+     */
+    public function iAmOnTheSearchPage()
     {
-        return md5($subjectName);
+        $this->visitPath('/search');
     }
 
     /**
@@ -110,6 +328,26 @@ final class SearchContext extends Context
     }
 
     /**
+     * @When /^I search for \'([^\']*)\'$/
+     */
+    public function iSearchFor(string $keyword)
+    {
+        $this->iClickSearch();
+
+        $this->getSession()->getPage()->fillField('for', $keyword);
+
+        $this->getSession()->getPage()->pressButton('Search');
+    }
+
+    /**
+     * @When /^I load more results$/
+     */
+    public function iLoadMoreResults()
+    {
+        $this->getSession()->getPage()->clickLink('Load more');
+    }
+
+    /**
      * @Then /^I should see the option to limit the search to \'([^\']*)\'$/
      */
     public function iShouldSeeTheOptionToLimitTheSearchTo(string $subject)
@@ -117,5 +355,31 @@ final class SearchContext extends Context
         $this->spin(function () use ($subject) {
             $this->assertSession()->fieldExists('Limit my search to '.$subject);
         });
+    }
+
+    /**
+     * @Then /^I should see the (\d+) most relevant results for \'([^\']*)\'$/
+     */
+    public function iShouldSeeTheMostRelevantResultsFor(int $number, string $keyword)
+    {
+        $this->spin(function () use ($number) {
+            $this->assertSession()->elementsCount('css', '.message-bar:contains("'.$this->numberOfArticles.' results found") + .list-heading + .listing-list > .listing-list__item', $number);
+
+            for ($i = $number; $i > 0; --$i) {
+                $nthChild = ($number - $i + 1);
+                $expectedNumber = ($this->numberOfArticles - $nthChild + 1);
+
+                $this->assertSession()->elementContains(
+                    'css',
+                    '.message-bar:contains("'.$this->numberOfArticles.' results found") + .list-heading + .listing-list > .listing-list__item:nth-child('.$nthChild.')',
+                    'Article '.str_pad($expectedNumber, 5, '0', STR_PAD_LEFT).' title'
+                );
+            }
+        });
+    }
+
+    private function createSubjectId(string $subjectName) : string
+    {
+        return md5($subjectName);
     }
 }
