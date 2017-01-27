42779d22c6219ea169b702687a76a476f34868fa
Giorgio Sironi <info@giorgiosironi.com>
2017-01-27 09:03:30 +0000

    Script for executing gulp multiple times hoping their failures are independent (#223)
    * Script for executing gulp multiple times hoping their failures are independent
    
    * Using /usr/bin/env bash
    
    * shellcheck linting

---
+++ retrying-gulp.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+maximum=${1:-3}
+
+for attempt in $(seq 1 "$maximum"); do
+    echo "Attempt: $attempt"
+    if ! node_modules/.bin/gulp; then
+        echo "Gulp failure, retrying"
+        continue
+    else
+        echo "Gulp succeeded"
+        break
+    fi
+done
+echo "Giving up after $attempt consecutive failures"
