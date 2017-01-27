43816a1e85390553b848400031803364c6cc40ba
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-01-27 08:50:54 +0000

    Add Google Scholar and PubMed links to references

--- src/ViewModel/Converter/Reference/ThesisReferenceConverter.php
+++ src/ViewModel/Converter/Reference/ThesisReferenceConverter.php
@@ -21,7 +21,15 @@ final class ThesisReferenceConverter implements ViewModelConverter
             return ViewModel\Reference::withDoi($object->getTitle(), new ViewModel\Doi($object->getDoi()), [$object->getPublisher()->toString()], $authors);
         }
 
-        return ViewModel\Reference::withOutDoi(new ViewModel\Link($object->getTitle(), $object->getUri()), [$object->getPublisher()->toString()], $authors);
+        $query = [
+            'title' => strip_tags($object->getTitle()),
+            'author' => $object->getAuthor()->getPreferredName(),
+            'publication_year' => $object->getDate()->getYear(),
+        ];
+
+        $abstracts = [new ViewModel\Link('Google Scholar', 'https://scholar.google.com/scholar_lookup?'.str_replace(['%5B0%5D=', '%5B1%5D='], '=', http_build_query($query)))];
+
+        return ViewModel\Reference::withOutDoi(new ViewModel\Link($object->getTitle(), $object->getUri()), [$object->getPublisher()->toString()], $authors, $abstracts);
     }
 
     public function supports($object, string $viewModel = null, array $context = []) : bool
