a35707a264b3e915a12d6ec3e1d5164e0d41c4a5
Chris Wilkinson <chriswilkinson84@gmail.com>
2017-02-21 08:24:53 +0000

    Add press package page (#273)

--- src/Controller/PressPacksController.php
+++ src/Controller/PressPacksController.php
@@ -2,10 +2,15 @@
 
 namespace eLife\Journal\Controller;
 
+use eLife\ApiSdk\Model\PressPackage;
 use eLife\Journal\Helper\Callback;
 use eLife\Journal\Helper\HasPages;
+use eLife\Patterns\ViewModel\ArticleSection;
 use eLife\Patterns\ViewModel\ContentHeaderNonArticle;
+use eLife\Patterns\ViewModel\LeadParas;
+use eLife\Patterns\ViewModel\Listing;
 use eLife\Patterns\ViewModel\ListingTeasers;
+use eLife\Patterns\ViewModel\Teaser;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -65,6 +70,31 @@ final class PressPacksController extends Controller
 
         $arguments['contentHeader'] = $arguments['package']
             ->then($this->willConvertTo(ContentHeaderNonArticle::class));
+
+        $arguments['leadParas'] = $arguments['package']
+            ->then(Callback::methodEmptyOr('getImpactStatement', $this->willConvertTo(LeadParas::class)));
+
+        $arguments['blocks'] = $arguments['package']
+            ->then(function (PressPackage $package) {
+                $parts = $this->convertContent($package)->toArray();
+
+                if ($package->getMediaContacts()->notEmpty()) {
+                    $mediaContacts = Listing::ordered($package->getMediaContacts()->map($this->willConvertTo())->map($this->willRender())->toArray());
+
+                    $parts[] = ArticleSection::basic('Media contacts', 2, $this->render($mediaContacts));
+                }
+
+                if ($package->getAbout()->notEmpty()) {
+                    $parts[] = ArticleSection::basic('About', 2, $this->render(...$package->getAbout()->map($this->willConvertTo(null, ['level' => 2]))));
+                }
+
+                return $parts;
+            });
+
+        $arguments['relatedContent'] = $arguments['package']
+            ->then(function (PressPackage $package) {
+                return ListingTeasers::basic($package->getRelatedContent()->map($this->willConvertTo(Teaser::class, ['variant' => 'secondary']))->toArray());
+            });
 
         return new Response($this->get('templating')->render('::press-pack.html.twig', $arguments));
     }
