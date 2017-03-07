2ee0615cc84eab98b23d66ae340c2c965a3cae50
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-03-07 15:41:12 +0000

    Add search box to search page

--- src/ViewModel/Factory/SiteHeaderFactory.php
+++ src/ViewModel/Factory/SiteHeaderFactory.php
@@ -18,21 +18,64 @@ use eLife\Patterns\ViewModel\SiteHeader;
 use eLife\Patterns\ViewModel\SiteHeaderNavBar;
 use eLife\Patterns\ViewModel\SubjectFilter;
 use Puli\UrlGenerator\Api\UrlGenerator as PuliUrlGenerator;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 final class SiteHeaderFactory
 {
     private $urlGenerator;
     private $puliUrlGenerator;
+    private $requestStack;
 
-    public function __construct(UrlGeneratorInterface $urlGenerator, PuliUrlGenerator $puliUrlGenerator)
+    public function __construct(UrlGeneratorInterface $urlGenerator, PuliUrlGenerator $puliUrlGenerator, RequestStack $requestStack)
     {
         $this->urlGenerator = $urlGenerator;
         $this->puliUrlGenerator = $puliUrlGenerator;
+        $this->requestStack = $requestStack;
     }
 
     public function createSiteHeader(Model $model = null) : SiteHeader
     {
+        if ($this->requestStack->getCurrentRequest() && 'search' !== $this->requestStack->getCurrentRequest()->get('_route')) {
+            $searchItem = NavLinkedItem::asIcon(new Link('Search', $this->urlGenerator->generate('search')),
+                new Picture(
+                    [
+                        ['srcset' => $this->puliUrlGenerator->generateUrl('/elife/patterns/assets/img/patterns/molecules/nav-primary-search-ic.svg')],
+                    ],
+                    new Image(
+                        $this->puliUrlGenerator->generateUrl('/elife/patterns/assets/img/patterns/molecules/nav-primary-search-ic_1x.png'),
+                        [
+                            48 => $this->puliUrlGenerator->generateUrl('/elife/patterns/assets/img/patterns/molecules/nav-primary-search-ic_2x.png'),
+                            24 => $this->puliUrlGenerator->generateUrl('/elife/patterns/assets/img/patterns/molecules/nav-primary-search-ic_1x.png'),
+                        ],
+                        'Search icon'
+                    )
+                ),
+                false,
+                true,
+                'search'
+            );
+        } else {
+            $searchItem = NavLinkedItem::asIcon(new Link('Search'),
+                new Picture(
+                    [
+                        ['srcset' => $this->puliUrlGenerator->generateUrl('/elife/patterns/assets/img/patterns/molecules/nav-primary-search-disabled-ic.svg')],
+                    ],
+                    new Image(
+                        $this->puliUrlGenerator->generateUrl('/elife/patterns/assets/img/patterns/molecules/nav-primary-search-disabled-ic_1x.png'),
+                        [
+                            48 => $this->puliUrlGenerator->generateUrl('/elife/patterns/assets/img/patterns/molecules/nav-primary-search-disabled-ic_2x.png'),
+                            24 => $this->puliUrlGenerator->generateUrl('/elife/patterns/assets/img/patterns/molecules/nav-primary-search-disabled-ic_1x.png'),
+                        ],
+                        'Search icon'
+                    )
+                ),
+                false,
+                true,
+                'search'
+            );
+        }
+
         $primaryLinks = SiteHeaderNavBar::primary([
             NavLinkedItem::asIcon(
                 new Link('Menu', '#mainMenu'),
@@ -55,24 +98,7 @@ final class SiteHeaderFactory
             ),
             NavLinkedItem::asLink(new Link('Home', $this->urlGenerator->generate('home'))),
             NavLinkedItem::asLink(new Link('Magazine', $this->urlGenerator->generate('magazine'))),
-            NavLinkedItem::asIcon(new Link('Search', $this->urlGenerator->generate('search')),
-                new Picture(
-                    [
-                        ['srcset' => $this->puliUrlGenerator->generateUrl('/elife/patterns/assets/img/patterns/molecules/nav-primary-search-ic.svg')],
-                    ],
-                    new Image(
-                        $this->puliUrlGenerator->generateUrl('/elife/patterns/assets/img/patterns/molecules/nav-primary-search-ic_1x.png'),
-                        [
-                            48 => $this->puliUrlGenerator->generateUrl('/elife/patterns/assets/img/patterns/molecules/nav-primary-search-ic_2x.png'),
-                            24 => $this->puliUrlGenerator->generateUrl('/elife/patterns/assets/img/patterns/molecules/nav-primary-search-ic_1x.png'),
-                        ],
-                        'Search icon'
-                    )
-                ),
-                false,
-                true,
-                'search'
-            ),
+            $searchItem,
         ]);
 
         $secondaryLinks = SiteHeaderNavBar::secondary([
@@ -92,14 +118,18 @@ final class SiteHeaderFactory
             $subject = null;
         }
 
-        $searchBox = new SearchBox(
-            new CompactForm(
-                new Form($this->urlGenerator->generate('search'), 'search', 'GET'),
-                new Input('Search by keyword or author', 'search', 'for', null, 'Search by keyword or author'),
-                'Search'
-            ),
-            $subject ? new SubjectFilter('subjects[]', $subject->getId(), $subject->getName()) : null
-        );
+        if ($this->requestStack->getCurrentRequest() && 'search' !== $this->requestStack->getCurrentRequest()->get('_route')) {
+            $searchBox = new SearchBox(
+                new CompactForm(
+                    new Form($this->urlGenerator->generate('search'), 'search', 'GET'),
+                    new Input('Search by keyword or author', 'search', 'for', null, 'Search by keyword or author'),
+                    'Search'
+                ),
+                $subject ? new SubjectFilter('subjects[]', $subject->getId(), $subject->getName()) : null
+            );
+        } else {
+            $searchBox = null;
+        }
 
         return new SiteHeader($this->urlGenerator->generate('home'), $primaryLinks, $secondaryLinks, $searchBox);
     }
