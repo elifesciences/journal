ad2450e364d0ebab011b72c1bdc629dd029f6d69
Chris Wilkinson <chriswilkinson84@gmail.com>
2017-03-08 12:26:01 +0000

    Add button blocks (#303)

---
+++ src/ViewModel/Converter/Block/ButtonConverter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace eLife\Journal\ViewModel\Converter\Block;
+
+use eLife\ApiSdk\Model\Block;
+use eLife\Journal\ViewModel\Converter\ViewModelConverter;
+use eLife\Patterns\ViewModel;
+
+final class ButtonConverter implements ViewModelConverter
+{
+    /**
+     * @param Block\Button $object
+     */
+    public function convert($object, string $viewModel = null, array $context = []) : ViewModel
+    {
+        return ViewModel\Button::link($object->getText(), $object->getUri());
+    }
+
+    public function supports($object, string $viewModel = null, array $context = []) : bool
+    {
+        return $object instanceof Block\Button;
+    }
+}
