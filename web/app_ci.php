cae84c48965878e7e6c6da4da79b777d7b4dc636
Chris Wilkinson <chriswilkinson84@gmail.com>
2017-03-16 08:01:28 +0000

    Use PHP7 optimisations (#322)

--- web/app_ci.php
+++ web/app_ci.php
@@ -8,6 +8,5 @@ require_once __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('ci', true);
-$kernel->loadClassCache();
 
 $kernel->run(Request::createFromGlobals());
--- web/app_end2end.php
+++ web/app_end2end.php
@@ -4,10 +4,8 @@ use eLife\Journal\AppKernel;
 use Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__.'/../app/autoload.php';
-require_once __DIR__.'/../var/bootstrap.php.cache';
 
 $kernel = new AppKernel('end2end', false);
-$kernel->loadClassCache();
 
 Request::enableHttpMethodParameterOverride();
 Request::setTrustedProxies([$_SERVER['REMOTE_ADDR']]); // ELB
