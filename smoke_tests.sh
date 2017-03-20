bc140650b9f528fa1802b0e52fb146b31de18e59
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-03-20 11:53:51 +0000

    Check favicons assets too

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -8,6 +8,7 @@ function from_manifest {
 bin/console --version --env=$ENVIRONMENT_NAME
 
 smoke_url_ok $(hostname)/favicon.ico
+smoke_url_ok $(hostname)/$(from_manifest assets/favicons/manifest.json)
 smoke_url_ok $(hostname)/$(from_manifest assets/patterns/css/all.css)
 smoke_url_ok $(hostname)/$(from_manifest assets/images/banners/magazine-hi-res.jpg)
 smoke_url_ok $(hostname)/ping
