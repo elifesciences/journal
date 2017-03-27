3d9c4f12730438786db77db7b7f83595f59ba9d6
Chris Wilkinson <chriswilkinson84@gmail.com>
2017-03-27 07:43:26 +0100

    Add article metric charts (#350)

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -391,10 +391,12 @@ final class ArticlesController extends Controller
 
                 if ($pageViews) {
                     $statistics[] = ViewModel\Statistic::fromNumber('Page views', $pageViews);
+                    $statisticsExtra[] = new ViewModel\BarChart($article->getId(), 'article', 'page-views', rtrim($this->getParameter('api_url'), '/'), 'page-views', 'month');
                 }
 
                 if ($downloads) {
                     $statistics[] = ViewModel\Statistic::fromNumber('Downloads', $downloads);
+                    $statisticsExtra[] = new ViewModel\BarChart($article->getId(), 'article', 'downloads', rtrim($this->getParameter('api_url'), '/'), 'downloads', 'month');
                 }
 
                 if ($citations) {
