616ad6ff0c764b78eda96b3f1a3f0a2f8ca4a230
Paul Shannon <p.shannon@elifesciences.org>
2017-04-06 11:24:45 +0100

    Added privacy policy content

--- src/Controller/TermsController.php
+++ src/Controller/TermsController.php
@@ -259,7 +259,7 @@ final class TermsController extends Controller
             ),
             ArticleSection::basic('Questions and Comments', 3,
                 $this->render(
-                    new Paragraph('f you have any questions or comments about these Terms and Conditions,
+                    new Paragraph('If you have any questions or comments about these Terms and Conditions,
                       please contact <a href="mailto:info@elifesciences.org">info@elifesciences.org</a>')
                 )
             ),
@@ -273,6 +273,47 @@ final class TermsController extends Controller
             ArticleSection::basic('Website visitors', 3,
                 $this->render(
                     new Paragraph('Like most Website operators, eLife collects non-personally identifying information of the sort that Web browsers and servers typically make available, such as the browser type, language preference, referring site, and the date and time of each visitor request. eLife&rsquo;s purpose in collecting non-personally identifying information is to better understand how eLife&rsquo;s visitors use its Website. From time to time, eLife may release non-personally identifying information in the aggregate, e.g., by publishing a report on trends in the usage of its Website. eLife also collects potentially personally identifying information like Internet Protocol (IP) addresses for logged in users and for users leaving comments on our site. eLife only discloses logged in user and commenter IP addresses under the same circumstances that it uses and discloses personally identifying information as described below, except that commenter IP addresses are visible and disclosed to the administrator.')
+                )
+            ),
+            ArticleSection::basic('Gathering of personally identifying information', 3,
+                $this->render(
+                    new Paragraph('Certain visitors to eLife&rsquo;s Websites choose to interact with eLife in ways that require eLife to gather personally identifying information. The amount and type of information that eLife gathers depends on the nature of the interaction. For example, eLife operates a permission-based email communication system, which allows us to collect personal information and send emails to people who want to receive them. In each case, eLife collects such information only insofar as is necessary or appropriate to fulfil the purpose of the visitor&rsquo;s interaction with eLife. eLife does not disclose personally identifying information other than as described below. And visitors can always refuse to supply personally identifying information, with the caveat that it may prevent them from engaging in certain Website-related activities.')
+                )
+            ),
+            ArticleSection::basic('Aggregated statistics', 3,
+                $this->render(
+                    new Paragraph('eLife may collect statistics about the behavior of visitors to its Websites. eLife may display this information publicly or provide it to others. However, eLife does not disclose personally identifying information other than as described below.')
+                )
+            ),
+            ArticleSection::basic('Protection of certain personally identifying information', 3,
+                $this->render(
+                    new Paragraph('eLife discloses potentially personally identifying and personally identifying information only to those of its employees, contractors and affiliated organizations that (i) need to know that information in order to process it on eLife&rsquo;s behalf or to provide services available at eLife&rsquo;s Websites, and (ii) that have agreed not to disclose it to others. Some of those employees, contractors and affiliated organizations may be located outside of your home country; by using eLife&rsquo;s Websites, you consent to the transfer of such information to them. eLife will not rent or sell potentially personally identifying and personally identifying information to anyone. Other than to its employees, contractors and affiliated organizations, as described above, eLife discloses potentially personally identifying and personally identifying information only in response to a subpoena, court order or other governmental request, or when eLife believes in good faith that disclosure is reasonably necessary to protect the property or rights of eLife, third parties or the public at large. If you are a registered user of an eLife Website and have supplied your email address, eLife may occasionally send you an email to tell you about new features, solicit your feedback, or just keep you up to date with what&rsquo;s going on with eLife and our publications &ndash; based on the preferences expressed at email sign-up. eLife takes all measures reasonably necessary to protect against the unauthorized access, use, alteration or destruction of potentially personally identifying and personally identifying information.')
+                )
+            ),
+            ArticleSection::basic('Cookies', 3,
+                $this->render(
+                    new Paragraph('A cookie is a string of information that a Website stores on a visitor&rsquo;s computer, and that the visitor&rsquo;s browser provides to the Website each time the visitor returns. eLife uses cookies to help eLife identify and track visitors, their usage of eLife Website, and their Website access preferences. eLife visitors who do not wish to have cookies placed on their computers should set their browsers to refuse cookies before using eLife&rsquo;s Websites, with the drawback that certain features of eLife&rsquo;s Websites may not function properly without the aid of cookies. You can modify your browser settings to notify you each time a cookie is sent to it and you can decide whether or not to accept. Alternatively, you can set your browser to refuse all cookies. Cookies that have already been set can be deleted at any time using your browser settings. The independent website at <a href="http://www.aboutcookies.org/">www.aboutcookies.org</a> contains comprehensive information on how to modify your browser settings.')
+                )
+            ),
+            ArticleSection::basic('Business transfers', 3,
+                $this->render(
+                    new Paragraph('If eLife, or substantially all of its assets, were acquired, or in the unlikely event that eLife goes out of business or enters bankruptcy, user information will be one of the assets that is likely to be transferred to, or acquired by, a third party. You acknowledge that such transfers may occur, and you consent to eLife&#39;s transferring your user information to any acquirer of eLife and their continuing to use your personal information as set forth in this policy.')
+                )
+            ),
+            ArticleSection::basic('Privacy policy changes', 3,
+                $this->render(
+                    new Paragraph('Although most changes are likely to be minor, eLife may change its Privacy Policy from time to time, and at eLife&rsquo;s sole discretion. eLife encourages visitors to frequently check this page for any changes to its Privacy Policy. Your continued use of this site after any change in this Privacy Policy will constitute your acceptance of such change. <em>First published March 30, 2012.</em> Change log:')
+                ) .
+                $this->render(
+                    Listing::unordered([
+                        'Added para&#39;s 2-3 on cookies (5 Dec, 12)',
+                        'Removed section on advertisers (3 Apr, 12)',
+                    ], 'bullet')
+                )
+            ),
+            ArticleSection::basic('Privacy policy attribution and reuse', 3,
+                $this->render(
+                    new Paragraph('This policy is based on the one developed by <a href="http://automattic.com/privacy/" target="_blank">Automattic.com</a> and made available under a <a href="http://creativecommons.org/licenses/by-sa/3.0/" target="_blank">CC-SA license</a>.')
                 )
             ),
         ];
