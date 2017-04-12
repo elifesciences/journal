132e872f5f0db6919bdb2272822e4c0d2d651cbf
Paul Shannon <p.shannon@elifesciences.org>
2017-04-12 10:29:40 +0100

    Fixes for missing HTTPS, medium URL and routing to RSS

--- src/Controller/AlertsController.php
+++ src/Controller/AlertsController.php
@@ -29,13 +29,13 @@ final class AlertsController extends Controller
             ArticleSection::basic('New Research', 2, $this->render(
                 Listing::unordered([
                     'Use our <a href="https://crm.elifesciences.org/crm/node/3">email preference management center</a> to sign up for weekly notifications of new published research plus papers available in PDF shortly after acceptance',
-                    'To stay on top of new research available every day, subscribe to our RSS feeds for <a href="https://elifesciences.org/rss/ahead.xml">author PDFs</a> and <a href="https://elifesciences.org/rss/recent.xml">published articles</a>',
+                    'To stay on top of new research available every day, subscribe to our RSS feeds for <a href="'.$this->get('router')->generate('rss-ahead').'">author PDFs</a> and <a href="'.$this->get('router')->generate('rss-recent').'">published articles</a>',
                     'See the highlights of recently published research and more on <a href="https://www.twitter.com/elife">Twitter</a> or <a href="https://www.facebook.com/elifesciences">Facebook</a>',
                 ], 'bullet')
             )),
             ArticleSection::basic('Science in plain language', 2, $this->render(
                 Listing::unordered([
-                    '<a href="https://medium.com/feed/@eLIfe">Subscribe</a> to the RSS feed for eLife Digests on Medium.com',
+                    '<a href="https://medium.com/feed/@eLife">Subscribe to the RSS feed for eLife Digests</a> on Medium.com',
                 ], 'bullet')
             )),
             ArticleSection::basic('Community-building', 2, $this->render(
@@ -48,7 +48,7 @@ final class AlertsController extends Controller
                     'For the latest in eLife Labs, innovation, and new tools, <a href="https://crm.elifesciences.org/crm/node/8">sign up for our technology and innovation newsletter</a>',
                 ], 'bullet')
             )),
-            new Paragraph('eLife is also on <a href="http://www.linkedin.com/company/elife-sciences-publications-ltd">LinkedIn</a> and <a href="https://plus.google.com/102129675554093758550/posts">Google Plus</a>.'),
+            new Paragraph('eLife is also on <a href="https://www.linkedin.com/company/elife-sciences-publications-ltd">LinkedIn</a> and <a href="https://plus.google.com/102129675554093758550/posts">Google Plus</a>.'),
         ];
 
         return new Response($this->get('templating')->render('::alerts.html.twig', $arguments));
