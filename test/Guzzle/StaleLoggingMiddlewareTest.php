4f65e5864fa235c71cc9f878203d386c412798c0
Giorgio Sironi <info@giorgiosironi.com>
2017-05-08 13:46:20 +0100

    Do not log stale errors in case there is no max-age required (#449)
    * Do not log stale errors in case there is no max-age required
    
    Still useful to log at the debug level, which shouldn't be used in production
    
    * Only logging locally-stale responses
    
    * Declare properties
    
    * Remove unused method
    
    * Restore not logging revalidating responses as errors

--- test/Guzzle/StaleLoggingMiddlewareTest.php
+++ test/Guzzle/StaleLoggingMiddlewareTest.php
@@ -5,12 +5,24 @@ namespace test\eLife\Journal\Guzzle;
 use eLife\Journal\Guzzle\StaleLoggingMiddleware;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
+use Kevinrob\GuzzleCache\CacheMiddleware;
 use PHPUnit_Framework_TestCase;
 use Psr\Log\LoggerInterface;
 use function GuzzleHttp\Promise\promise_for;
 
 final class StaleLoggingMiddlewareTest extends PHPUnit_Framework_TestCase
 {
+    /** @var LoggerInterface */
+    private $logger;
+    /** @var StaleLoggingMiddleware */
+    private $middleware;
+
+    public function setUp()
+    {
+        $this->logger = $this->createMock(LoggerInterface::class);
+        $this->middleware = new StaleLoggingMiddleware($this->logger);
+    }
+
     /**
      * @test
      */
@@ -18,18 +30,15 @@ final class StaleLoggingMiddlewareTest extends PHPUnit_Framework_TestCase
     {
         $request = new Request('GET', 'http://www.example.com/');
         $options = [];
-        $response = new Response(200, ['Age' => '10', 'Cache-Control' => 'max-age=5']);
+        $response = new Response(200, ['Age' => '10', CacheMiddleware::HEADER_CACHE_INFO => CacheMiddleware::HEADER_CACHE_STALE]);
 
-        $logger = $this->createMock(LoggerInterface::class);
-
-        $middleware = new StaleLoggingMiddleware($logger);
         $handler = function () use ($response) {
             return promise_for($response);
         };
 
-        $promise = call_user_func($middleware($handler), $request, $options);
+        $promise = call_user_func($this->middleware->__invoke($handler), $request, $options);
 
-        $logger->expects($this->once())
+        $this->logger->expects($this->once())
             ->method('error')
             ->with($this->identicalTo('Using stale response for GET http://www.example.com/'));
 
@@ -39,21 +48,38 @@ final class StaleLoggingMiddlewareTest extends PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function it_does_not_log_if_the_response_is_fresh()
+    public function it_does_not_log_if_the_response_is_a_hit_that_is_fresh()
     {
         $request = new Request('GET', 'http://www.example.com/');
         $options = [];
-        $response = new Response(200, ['Age' => '5', 'Cache-Control' => 'max-age=10']);
+        $response = new Response(200, [CacheMiddleware::HEADER_CACHE_INFO => CacheMiddleware::HEADER_CACHE_HIT]);
 
-        $logger = $this->createMock(LoggerInterface::class);
-        $logger->expects($this->never())->method($this->anything());
+        $this->logger->expects($this->never())->method($this->anything());
 
-        $middleware = new StaleLoggingMiddleware($logger);
         $handler = function () use ($response) {
             return promise_for($response);
         };
 
-        $promise = call_user_func($middleware($handler), $request, $options);
+        $promise = call_user_func($this->middleware->__invoke($handler), $request, $options);
+        $promise->wait();
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_log_if_the_response_is_a_miss_and_hence_is_fresh()
+    {
+        $request = new Request('GET', 'http://www.example.com/');
+        $options = [];
+        $response = new Response(200, [CacheMiddleware::HEADER_CACHE_INFO => CacheMiddleware::HEADER_CACHE_MISS]);
+
+        $this->logger->expects($this->never())->method($this->anything());
+
+        $handler = function () use ($response) {
+            return promise_for($response);
+        };
+
+        $promise = call_user_func($this->middleware->__invoke($handler), $request, $options);
         $promise->wait();
     }
 
@@ -64,18 +90,15 @@ final class StaleLoggingMiddlewareTest extends PHPUnit_Framework_TestCase
     {
         $request = new Request('GET', 'http://www.example.com/');
         $options = [];
-        $response = new Response(200, ['Age' => '10', 'Cache-Control' => 'max-age=5, stale-while-revalidate=5']);
+        $response = new Response(200, ['Age' => '10', 'Cache-Control' => 'max-age=5, stale-while-revalidate=5', CacheMiddleware::HEADER_CACHE_INFO => CacheMiddleware::HEADER_CACHE_STALE]);
 
-        $logger = $this->createMock(LoggerInterface::class);
-
-        $middleware = new StaleLoggingMiddleware($logger);
         $handler = function () use ($response) {
             return promise_for($response);
         };
 
-        $promise = call_user_func($middleware($handler), $request, $options);
+        $promise = call_user_func($this->middleware->__invoke($handler), $request, $options);
 
-        $logger->expects($this->once())
+        $this->logger->expects($this->once())
             ->method('info')
             ->with($this->identicalTo('Using stale response for GET http://www.example.com/'));
 
