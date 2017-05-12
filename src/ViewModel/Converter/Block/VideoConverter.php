eafa1f88f5cff383669d74838d01233107160a19
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-05-12 15:53:18 +0100

    Fix VideoConverter

--- src/ViewModel/Converter/Block/VideoConverter.php
+++ src/ViewModel/Converter/Block/VideoConverter.php
@@ -45,7 +45,7 @@ final class VideoConverter implements ViewModelConverter
 
     public function supports($object, string $viewModel = null, array $context = []) : bool
     {
-        return $object instanceof Block\Video && !$object->getTitle();
+        return $object instanceof Block\Video;
     }
 
     protected function getViewModelConverter() : ViewModelConverter
