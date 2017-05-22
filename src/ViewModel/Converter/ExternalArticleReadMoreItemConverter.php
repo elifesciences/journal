5ce5b260f276c3a6f550fb7c55eeb07d8419696f
Giorgio Sironi <info@giorgiosironi.com>
2017-05-22 07:31:05 +0100

    External articles may appear as Read More items (#526)
    * External articles may appear as Read More items
    
    (if they come out of recommendations, not necessarily in an high place as retractions/corrections may come first)
    
    * Review changes

---
+++ src/ViewModel/Converter/ExternalArticleReadMoreItemConverter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace eLife\Journal\ViewModel\Converter;
+
+use eLife\ApiSdk\Model\ExternalArticle;
+use eLife\Patterns\ViewModel;
+
+final class ExternalArticleReadMoreItemConverter implements ViewModelConverter
+{
+    /**
+     * @param ExternalArticle $object
+     */
+    public function convert($object, string $viewModel = null, array $context = []) : ViewModel
+    {
+        return new ViewModel\ReadMoreItem(
+            new ViewModel\ContentHeaderReadMore(
+                $object->getTitle(),
+                $object->getUri(),
+                [],
+                $object->getAuthorLine(),
+                ViewModel\Meta::withText($object->getJournal())
+            )
+        );
+    }
+
+    public function supports($object, string $viewModel = null, array $context = []) : bool
+    {
+        return $object instanceof ExternalArticle && ViewModel\ReadMoreItem::class === $viewModel;
+    }
+}
