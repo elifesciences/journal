8c6bdd3fbe78ab72888e5d96b9a1d4e45475079c
Chris Wilkinson <chriswilkinson84@gmail.com>
2017-05-24 09:42:45 +0100

    Add for the press text (#547)

--- src/Controller/PressPacksController.php
+++ src/Controller/PressPacksController.php
@@ -5,6 +5,7 @@ namespace eLife\Journal\Controller;
 use eLife\ApiSdk\Model\PressPackage;
 use eLife\Journal\Helper\Callback;
 use eLife\Journal\Helper\HasPages;
+use eLife\Journal\ViewModel\Paragraph;
 use eLife\Patterns\ViewModel\ArticleSection;
 use eLife\Patterns\ViewModel\ContentHeader;
 use eLife\Patterns\ViewModel\Listing;
@@ -52,6 +53,14 @@ final class PressPacksController extends Controller
     private function createFirstPage(array $arguments) : Response
     {
         $arguments['contentHeader'] = new ContentHeader($arguments['title']);
+
+        $arguments['sidebar'] = [
+            new Paragraph('Inquiries about published papers, material in press and eLife may be directed to <a href="mailto:press@elifesciences.org">press@elifesciences.org</a>, or +44&nbsp;1223&nbsp;855373.'),
+            new Paragraph('If you’re writing about an eLife study, please cite eLife as the source of the article and include a link to either the article or elifesciences.org, preferably using our DOI: <code>https://doi.org/10.7554/eLife</code> – with the article’s five-digit extension (e.g. <code>https://doi.org/10.7554/eLife.00000</code>). Thank you!'),
+            new Paragraph('All content, unless otherwise stated, is available under a <a href="https://creativecommons.org/licenses/by/4.0">Creative Commons Attribution License (CC-BY)</a>. All are free to use and reuse the content provided the original source and authors are credited.'),
+            new Paragraph('The eLife media policy, encouraging authors to present and discuss their works ahead of publication and indicating that eLife will not release content under embargo, is available in full in our <a href="https://submit.elifesciences.org/html/elife_author_instructions.html#policies">Journal Policies</a>.'),
+            new Paragraph('Please don’t hesitate to contact us if you have any questions.'),
+        ];
 
         return new Response($this->get('templating')->render('::press-packs.html.twig', $arguments));
     }
