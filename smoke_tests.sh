164e5059b92c05934784bd6087a0ee1ec0696851
Giorgio Sironi <g.sironi@elifesciences.org>
2017-05-26 16:35:31 +0000

    Asserting on both good and bad status checks

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -19,9 +19,13 @@ if [ "$ENVIRONMENT_NAME" != "ci" ] && [ "$ENVIRONMENT_NAME" != "dev" ]
     checks=$(curl -v $(hostname)/status | grep check__name)
     echo $checks
     good_checks=$(echo $checks | grep -o ✔ | wc -l)
-    if [ "$good_checks" -ne 6 ]; then
+    bad_checks=$(echo $checks | grep -o ✘ | wc -l)
+    if [ "$bad_checks" -ne 0 ]; then
         echo "Not all status checks are ok"
         exit 2
+    elif [ "$good_checks" -eq 0 ]; then
+        echo "There is not even one status check"
+        exit 3
     else
         echo "All checks are ok"
     fi
