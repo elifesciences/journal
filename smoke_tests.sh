b007d7214456fa12e2b21cf2c703bd1b56e53953
Giorgio Sironi <info@giorgiosironi.com>
2017-06-02 11:05:05 +0100

    Smoke tests: make only one request to /status (#568)
    * Only one /status request in smoke tests
    
    And that is the request that gets parsed and checked. Smoke testing needs an overhaul but this is useful for immediate feedback
    
    * Asserting on both good and bad status checks

--- smoke_tests.sh
+++ smoke_tests.sh
@@ -16,8 +16,19 @@ smoke_url_ok $(hostname)/ping
 
 if [ "$ENVIRONMENT_NAME" != "ci" ] && [ "$ENVIRONMENT_NAME" != "dev" ]
   then
-    curl -v $(hostname)/status | grep check__name
-    smoke_url_ok $(hostname)/status
+    checks=$(curl -v $(hostname)/status | grep check__name)
+    echo $checks
+    good_checks=$(echo $checks | grep -o ✔ | wc -l)
+    bad_checks=$(echo $checks | grep -o ✘ | wc -l)
+    if [ "$bad_checks" -ne 0 ]; then
+        echo "Not all status checks are ok"
+        exit 2
+    elif [ "$good_checks" -eq 0 ]; then
+        echo "There is not even one status check"
+        exit 3
+    else
+        echo "All checks are ok"
+    fi
     smoke_url_ok $(hostname)/
 fi
 
