561f9cd68f0ca903bc9b80c9db8a1a5e1de2f9b9
Chris Wilkinson <chriswilkinson84@gmail.com>
2017-06-05 07:46:30 +0100

    Link audio player title on the Magazine page (#605)
    * Link audio player title on the Magazine page
    
    * Fix test

--- src/ViewModel/Converter/PodcastEpisodeAudioPlayerConverter.php
+++ src/ViewModel/Converter/PodcastEpisodeAudioPlayerConverter.php
@@ -6,14 +6,17 @@ use eLife\ApiSdk\Model\PodcastEpisode;
 use eLife\ApiSdk\Model\PodcastEpisodeChapter;
 use eLife\ApiSdk\Model\PodcastEpisodeSource;
 use eLife\Patterns\ViewModel;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 final class PodcastEpisodeAudioPlayerConverter implements ViewModelConverter
 {
     private $viewModelConverter;
+    private $urlGenerator;
 
-    public function __construct(ViewModelConverter $viewModelConverter)
+    public function __construct(ViewModelConverter $viewModelConverter, UrlGeneratorInterface $urlGenerator)
     {
         $this->viewModelConverter = $viewModelConverter;
+        $this->urlGenerator = $urlGenerator;
     }
 
     /**
@@ -21,9 +24,13 @@ final class PodcastEpisodeAudioPlayerConverter implements ViewModelConverter
      */
     public function convert($object, string $viewModel = null, array $context = []) : ViewModel
     {
+        if ($context['link'] ?? false) {
+            $link = $this->urlGenerator->generate('podcast-episode', ['number' => $object->getNumber()]);
+        }
+
         return new ViewModel\AudioPlayer(
             $object->getNumber(),
-            $object->getTitle(),
+            new ViewModel\Link($object->getTitle(), $link ?? null),
             array_map(function (PodcastEpisodeSource $source) {
                 return new ViewModel\MediaSource($source->getUri(), new ViewModel\MediaType($source->getMediaType()));
             }, $object->getSources()),
