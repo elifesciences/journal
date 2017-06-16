a3360de85bf29368345f0625675b13032d4834a0
Giorgio Sironi <info@giorgiosironi.com>
2017-06-16 11:36:04 +0100

    New environment: baseline (#620)
    To be used by thedailybugle.org as the simplest configuration to deploy on AWS

---
+++ web/app_baseline.php
@@ -0,0 +1,20 @@
+<?php
+
+use eLife\Journal\AppKernel;
+use Symfony\Component\HttpFoundation\Request;
+
+require_once __DIR__.'/../app/autoload.php';
+
+$kernel = new AppKernel('baseline', false);
+
+Request::enableHttpMethodParameterOverride();
+
+// ELB
+Request::setTrustedProxies([$_SERVER['REMOTE_ADDR']]);
+Request::setTrustedHeaderName(Request::HEADER_FORWARDED, null);
+Request::setTrustedHeaderName(Request::HEADER_CLIENT_IP, 'X-Forwarded-For');
+Request::setTrustedHeaderName(Request::HEADER_CLIENT_HOST, null);
+Request::setTrustedHeaderName(Request::HEADER_CLIENT_PORT, 'X-Forwarded-Port');
+Request::setTrustedHeaderName(Request::HEADER_CLIENT_PROTO, 'X-Forwarded-Proto');
+
+$kernel->run(Request::createFromGlobals());
