b9bc80f809db48a7e1d2813fc229115aa7e71c08
Giorgio Sironi <g.sironi@elifesciences.org>
2017-07-10 09:48:04 +0000

    Echoing a title for each project tests step

--- project_tests.sh
+++ project_tests.sh
@@ -2,14 +2,19 @@
 set -e
 
 rm -f build/*.xml
+
+echo "cache:clear"
 bin/console cache:clear --env=test --no-warmup
-if bin/console security:check; then
-    echo "security:check passed"
-else
-    echo "security:check failed"
-    exit 2
-fi
+
+echo "security:check"
+bin/console security:check
+
+echo "proofreader"
 proofreader app/ bin/ src/ web/
 proofreader --no-phpcpd features/ test/
+
+echo "PHPUnit tests"
 vendor/bin/phpunit --log-junit build/phpunit.xml
+
+echo "Behat tests"
 vendor/bin/behat --strict --tags '~wip' --format junit --format progress
