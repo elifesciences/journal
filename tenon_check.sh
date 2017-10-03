f030bce9853657b7b9d44ffebbedbe03ba60a4a3
Giorgio Sironi <g.sironi@elifesciences.org>
2017-10-03 16:55:24 +0100

    shellcheck suggestions

--- tenon_check.sh
+++ tenon_check.sh
@@ -9,13 +9,13 @@ fi
 
 log="$1"
 
-api_errors=$(cat "$log" | jq '.apiErrors | length')
+api_errors=$(jq '.apiErrors | length' < "$log")
 if [ "$api_errors" -ne 0 ]; then
     echo "apiErrors is not empty in $log"
     exit 2
 fi
 
-result_set=$(cat "$log" | jq '.resultSet | length')
+result_set=$(jq '.resultSet | length' < "$log")
 if [ "$result_set" -ne 0 ]; then
     echo "resultSet is not empty ($result_set issues) in $log"
     exit 3
--- tenon_key_pages.sh
+++ tenon_key_pages.sh
@@ -3,14 +3,14 @@ set -e
 
 rm -rf build/tenon/*.log
 
-for path in $(cat tenon_key_pages.txt)
+while read path
 do
     log_file=$(echo "$path" | tr / _).log
     ./tenon.sh "$path" | tee "build/tenon/$log_file"
-done
+done < tenon_key_pages.txt
 
 # will stop on first failure, but all logs are available
-for log in $(ls build/tenon/*.log)
+for log in build/tenon/*.log
 do
     ./tenon_check.sh "$log"
 done
