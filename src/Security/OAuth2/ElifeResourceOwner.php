20f94e4c2c5a96af775b3edf5dd35e7aeedd0fc7
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-10-16 09:13:48 +0100

    Show error message when authentication fails

--- src/Security/OAuth2/ElifeResourceOwner.php
+++ src/Security/OAuth2/ElifeResourceOwner.php
@@ -2,9 +2,12 @@
 
 namespace eLife\Journal\Security\OAuth2;
 
+use Http\Client\Exception\HttpException;
+use HWI\Bundle\OAuthBundle\OAuth\Exception\HttpTransportException;
 use HWI\Bundle\OAuthBundle\OAuth\ResourceOwner\GenericOAuth2ResourceOwner;
 use HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 final class ElifeResourceOwner extends GenericOAuth2ResourceOwner
 {
@@ -27,12 +30,25 @@ final class ElifeResourceOwner extends GenericOAuth2ResourceOwner
      */
     protected function doGetTokenRequest($url, array $parameters = [])
     {
-        return $this->httpRequest(
-            $url,
-            http_build_query($parameters, '', '&'),
-            ['Content-Type' => 'application/x-www-form-urlencoded'],
-            'POST'
-        );
+        try {
+            return $this->httpRequest(
+                $url,
+                http_build_query($parameters, '', '&'),
+                ['Content-Type' => 'application/x-www-form-urlencoded'],
+                'POST'
+            );
+        } catch (HttpTransportException $e) {
+            $previous = $e->getPrevious();
+            if (false === $previous instanceof HttpException) {
+                throw $e;
+            }
+
+            if (false !== strpos($previous->getResponse()->getBody(), 'No name visible')) {
+                throw new AuthenticationException('No name visible', 0, $e);
+            }
+
+            throw $e;
+        }
     }
 
     protected function configureOptions(OptionsResolver $resolver)
