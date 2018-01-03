b2cb8fd1f9bf3e5422b3e2c690314a7293b0e36c
David Moulton <d.moulton@elifesciences.org>
2018-01-03 16:03:18 +0000

    Correct grammatical style

--- src/Controller/PressPacksController.php
+++ src/Controller/PressPacksController.php
@@ -59,7 +59,7 @@ final class PressPacksController extends Controller
         $arguments['contentHeader'] = new ContentHeader($arguments['title']);
 
         $arguments['sidebar'] = [
-            new Paragraph('Inquiries about published papers, material in press and eLife may be directed to <a href="mailto:press@elifesciences.org">press@elifesciences.org</a>, or +44&nbsp;1223&nbsp;855373.'),
+            new Paragraph('Enquiries about published papers, material in press and eLife may be directed to <a href="mailto:press@elifesciences.org">press@elifesciences.org</a>, or +44&nbsp;1223&nbsp;855373.'),
             new Paragraph('If you’re writing about an eLife study, please cite eLife as the source of the article and include a link to either the article or elifesciences.org, preferably using our DOI: https://doi.org/10.7554/eLife – with the article’s five-digit extension (e.g. https://doi.org/10.7554/eLife.00000). Thank you!'),
             new Paragraph('All content, unless otherwise stated, is available under a <a href="https://creativecommons.org/licenses/by/4.0">Creative Commons Attribution License (CC-BY)</a>. All are free to use and reuse the content provided the original source and authors are credited.'),
             new Paragraph('The eLife media policy, encouraging authors to present and discuss their works ahead of publication and indicating that eLife will not release content under embargo, can be found under the Media Policy heading in our <a href="https://submit.elifesciences.org/html/elife_author_instructions.html#policies">Journal Policies</a>.'),
