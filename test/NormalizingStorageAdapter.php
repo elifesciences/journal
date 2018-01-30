30d258c9209c954b701bbf402902c4de3a9901a6
Chris Wilkinson <chriswilkinson84@gmail.com>
2018-01-30 11:20:00 +0000

    Switch to knpuniversity/oauth2-client-bundle (#824)
    * Switch to knpuniversity/oauth2-client-bundle
    
    * CS fixes
    
    * Message is the same as error_description so it's redundant
    
    * Normalize mocked requests
    
    * Simpler
    
    * Use bundle's user provider
    
    * *Ahem*
    
    * Redundant
    
    * Match #836
    
    * Tis no more
    
    * Fix test case

---
+++ test/NormalizingStorageAdapter.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace test\eLife\Journal;
+
+use Csa\Bundle\GuzzleBundle\Cache\StorageAdapterInterface;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Uri;
+use GuzzleHttp\Psr7\UriNormalizer;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Throwable;
+use function GuzzleHttp\json_decode;
+use function GuzzleHttp\json_encode;
+
+final class NormalizingStorageAdapter implements StorageAdapterInterface
+{
+    const URI_FLAGS = UriNormalizer::PRESERVING_NORMALIZATIONS | UriNormalizer::SORT_QUERY_PARAMETERS;
+
+    private $storageAdapter;
+
+    public function __construct(StorageAdapterInterface $storageAdapter)
+    {
+        $this->storageAdapter = $storageAdapter;
+    }
+
+    public function fetch(RequestInterface $request)
+    {
+        return $this->storageAdapter->fetch($this->normalize($request));
+    }
+
+    public function save(RequestInterface $request, ResponseInterface $response)
+    {
+        $this->storageAdapter->save($this->normalize($request), $response);
+    }
+
+    private function normalize(RequestInterface $request) : RequestInterface
+    {
+        $headers = array_change_key_case($request->getHeaders());
+        $uri = UriNormalizer::normalize($request->getUri(), self::URI_FLAGS);
+        $body = $request->getBody()->__toString();
+
+        if ($body) {
+            try {
+                if ('application/x-www-form-urlencoded' === $request->getHeaderLine('Content-Type')) {
+                    $body = UriNormalizer::normalize(new Uri("'?{$body}"), self::URI_FLAGS)->getQuery();
+                } elseif (false !== strpos($request->getHeaderLine('Content-Type'), 'json')) {
+                    $body = json_encode(json_decode($body));
+                }
+            } catch (Throwable $e) {
+                // Do nothing.
+            }
+        }
+
+        return new Request(
+            $request->getMethod(),
+            $uri,
+            $headers,
+            $body,
+            $request->getProtocolVersion()
+        );
+    }
+}
--- test/WebTestCase.php
+++ test/WebTestCase.php
@@ -4,15 +4,16 @@ namespace test\eLife\Journal;
 
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
-use HWI\Bundle\OAuthBundle\Security\Core\Authentication\Token\OAuthToken;
-use HWI\Bundle\OAuthBundle\Security\Core\User\OAuthUser;
+use KnpU\OAuth2ClientBundle\Security\User\OAuthUser;
 use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
 use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken;
 
 abstract class WebTestCase extends BaseWebTestCase
 {
     use AppKernelTestCase;
+    use Assertions;
 
     final protected function logIn(Client $client)
     {
@@ -20,18 +21,7 @@ abstract class WebTestCase extends BaseWebTestCase
 
         $session = $client->getContainer()->get('session');
 
-        $token = new OAuthToken(
-            [
-                'access_token' => 'token',
-                'expires_in' => 3920,
-                'token_type' => 'Bearer',
-                'id' => 'jcarberry',
-                'orcid' => '0000-0002-1825-0097',
-                'name' => 'Josiah Carberry',
-            ],
-            ['ROLE_USER', 'ROLE_OAUTH_USER']
-        );
-        $token->setUser(new OAuthUser('jcarberry'));
+        $token = new PostAuthenticationGuardToken(new OAuthUser('jcarberry', $roles = ['ROLE_USER', 'ROLE_OAUTH_USER']), 'main', $roles);
 
         $session->set('_security_main', serialize($token));
         $session->save();
