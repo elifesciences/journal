5878ebaa9d7e86afb929d8838d2907a2758acd45
Giorgio Sironi <g.sironi@elifesciences.org>
2018-02-07 16:51:28 +0000

    Retry status checks
    Since these fail often, give them one more chance in case the timeout of a backend is temporary

---
+++ status_test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+. /opt/smoke.sh/smoke.sh
+
+checks=$(curl -v $(hostname)/status | grep check__name)
+echo $checks
+good_checks=$(echo $checks | grep -o ✔ | wc -l)
+bad_checks=$(echo $checks | grep -o ✘ | wc -l)
+if [ "$bad_checks" -ne 0 ]; then
+    echo "Not all status checks are ok"
+    exit 2
+elif [ "$good_checks" -eq 0 ]; then
+    echo "There is not even one status check"
+    exit 3
+else
+    echo "All checks are ok"
+fi
