d379530b5c34446168c2e7b803023bf117aa4a05
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-02-21 12:06:48 +0000

    Update About pages titles and menu

--- test/Controller/AboutEarlyCareerControllerTest.php
+++ test/Controller/AboutEarlyCareerControllerTest.php
@@ -14,7 +14,7 @@ final class AboutEarlyCareerControllerTest extends PageTestCase
         $crawler = $client->request('GET', $this->getUrl());
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $this->assertSame('Early-career scientists', $crawler->filter('.content-header__title')->text());
+        $this->assertSame('Early-careers', $crawler->filter('.content-header__title')->text());
     }
 
     /**
@@ -28,10 +28,10 @@ final class AboutEarlyCareerControllerTest extends PageTestCase
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
 
-        $this->assertSame('Early-career scientists | About | eLife', $crawler->filter('title')->text());
+        $this->assertSame('Early-careers | About | eLife', $crawler->filter('title')->text());
         $this->assertSame('/about/early-career', $crawler->filter('link[rel="canonical"]')->attr('href'));
         $this->assertSame('http://localhost/about/early-career', $crawler->filter('meta[property="og:url"]')->attr('content'));
-        $this->assertSame('Early-career scientists', $crawler->filter('meta[property="og:title"]')->attr('content'));
+        $this->assertSame('Early-careers', $crawler->filter('meta[property="og:title"]')->attr('content'));
         $this->assertSame('The community behind eLife wants to help address some of the pressures on early-career scientists', $crawler->filter('meta[property="og:description"]')->attr('content'));
         $this->assertSame('The community behind eLife wants to help address some of the pressures on early-career scientists', $crawler->filter('meta[name="description"]')->attr('content'));
         $this->assertSame('summary', $crawler->filter('meta[name="twitter:card"]')->attr('content'));
