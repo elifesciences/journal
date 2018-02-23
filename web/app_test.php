2ec3e3faf2aa7fce040a9723194d3f532c0e99f8
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-02-23 16:11:53 +0000

    Also update here

--- web/app_test.php
+++ web/app_test.php
@@ -8,14 +8,14 @@ use Symfony\Component\HttpFoundation\Request;
 require_once __DIR__.'/../app/autoload.php';
 Debug::enable();
 
-if (!empty($_GET[EnvCommandCreator::ENV_TEST_CHANNEL])) {
-    setcookie(EnvCommandCreator::ENV_TEST_CHANNEL, $_GET[EnvCommandCreator::ENV_TEST_CHANNEL]);
+if (!empty($_GET['JOURNAL_INSTANCE'])) {
+    setcookie('JOURNAL_INSTANCE', $_GET['JOURNAL_INSTANCE']);
     die;
 }
 
-if (!empty($_COOKIE[EnvCommandCreator::ENV_TEST_CHANNEL])) {
-    putenv(EnvCommandCreator::ENV_TEST_CHANNEL.'='.$_COOKIE[EnvCommandCreator::ENV_TEST_CHANNEL]);
-    unset($_COOKIE[EnvCommandCreator::ENV_TEST_CHANNEL]);
+if (!empty($_COOKIE['JOURNAL_INSTANCE'])) {
+    putenv('JOURNAL_INSTANCE'.'='.$_COOKIE['JOURNAL_INSTANCE']);
+    unset($_COOKIE['JOURNAL_INSTANCE']);
 }
 
 $kernel = new AppKernel('test', true);
