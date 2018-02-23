ddee80741ba3ca888c0d1ddb0b8c40260b95ca57
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-02-23 07:56:39 +0000

    Different environment

--- web/app_ci.php
+++ web/app_ci.php
@@ -1,11 +1,22 @@
 <?php
 
 use eLife\Journal\AppKernel;
+use Liuggio\Fastest\Process\EnvCommandCreator;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__.'/../app/autoload.php';
 Debug::enable();
+
+if (!empty($_GET[EnvCommandCreator::ENV_TEST_CHANNEL])) {
+    setcookie(EnvCommandCreator::ENV_TEST_CHANNEL, $_GET[EnvCommandCreator::ENV_TEST_CHANNEL]);
+    die;
+}
+
+if (!empty($_COOKIE[EnvCommandCreator::ENV_TEST_CHANNEL])) {
+    putenv(EnvCommandCreator::ENV_TEST_CHANNEL.'='.$_COOKIE[EnvCommandCreator::ENV_TEST_CHANNEL]);
+    unset($_COOKIE[EnvCommandCreator::ENV_TEST_CHANNEL]);
+}
 
 $kernel = new AppKernel('ci', true);
 
