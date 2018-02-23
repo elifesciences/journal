5381750de9e3f8ade5d36580ae927fccc720a197
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-02-23 07:44:34 +0000

    Get more output

--- project_tests.sh
+++ project_tests.sh
@@ -17,7 +17,7 @@ vendor/bin/fastest --no-interaction --xml phpunit.xml.dist "vendor/bin/phpunit -
 
 echo "Behat tests"
 rm -rf build/behat/
-vendor/bin/behat --list-scenarios --tags '~wip' | vendor/bin/fastest --no-interaction "JARNAIZ_JUNIT_OUTPUTDIR=build/behat JARNAIZ_JUNIT_FILENAME={n}.xml vendor/bin/behat --strict --format junit {};"
+vendor/bin/behat --list-scenarios --tags '~wip' | vendor/bin/fastest --no-interaction "JARNAIZ_JUNIT_OUTPUTDIR=build/behat JARNAIZ_JUNIT_FILENAME={n}.xml vendor/bin/behat --strict --format junit {};" -vv --no-errors-summary
 
 # Tenon disabled as flaky and not (yet) used much
 # echo "Tenon tests"
