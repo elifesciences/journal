5659d1baaab35de9f6862e64147d1a5a494206e7
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-02-23 08:03:40 +0000

    Match number of processes to compare with Paratest

--- project_tests.sh
+++ project_tests.sh
@@ -13,11 +13,11 @@ proofreader --no-phpcpd features/ test/
 
 echo "PHPUnit tests"
 rm -rf build/phpunit/
-vendor/bin/fastest --no-interaction --xml phpunit.xml.dist "vendor/bin/phpunit --log-junit build/phpunit/{n}.xml {};"
+vendor/bin/fastest --no-interaction --process 4 --xml phpunit.xml.dist "vendor/bin/phpunit --log-junit build/phpunit/{n}.xml {};"
 
 echo "Behat tests"
 rm -rf build/behat/
-vendor/bin/behat --list-scenarios --tags '~wip' | vendor/bin/fastest --no-interaction "JARNAIZ_JUNIT_OUTPUTDIR=build/behat JARNAIZ_JUNIT_FILENAME={n}.xml vendor/bin/behat --strict --format junit {};" -vv --no-errors-summary
+vendor/bin/behat --list-scenarios --tags '~wip' | vendor/bin/fastest --no-interaction --process 4 "JARNAIZ_JUNIT_OUTPUTDIR=build/behat JARNAIZ_JUNIT_FILENAME={n}.xml vendor/bin/behat --strict --format junit {};"
 
 # Tenon disabled as flaky and not (yet) used much
 # echo "Tenon tests"
