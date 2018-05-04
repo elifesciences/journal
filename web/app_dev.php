c1ff3a0cbe33daf20279244e00049b30a53f0062
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-05-04 12:49:16 +0100

    Start on Behat

--- web/app_dev.php
+++ web/app_dev.php
@@ -7,6 +7,16 @@ use Symfony\Component\HttpFoundation\Request;
 require_once __DIR__.'/../vendor/autoload.php';
 Debug::enable();
 
+if (!empty($_GET['JOURNAL_INSTANCE'])) {
+    setcookie('JOURNAL_INSTANCE', $_GET['JOURNAL_INSTANCE']);
+    die;
+}
+
+if (!empty($_COOKIE['JOURNAL_INSTANCE'])) {
+    putenv('JOURNAL_INSTANCE'.'='.$_COOKIE['JOURNAL_INSTANCE']);
+    unset($_COOKIE['JOURNAL_INSTANCE']);
+}
+
 $kernel = new AppKernel('dev', true);
 
 $kernel->run(Request::createFromGlobals());
