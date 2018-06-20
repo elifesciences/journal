6768f11818f0b581b0db51c2890b50f11b9330ed
David Moulton <d.moulton@elifesciences.org>
2018-06-20 16:19:12 +0100

    Update links in site header navigation

--- src/ViewModel/Factory/SiteHeaderFactory.php
+++ src/ViewModel/Factory/SiteHeaderFactory.php
@@ -104,21 +104,21 @@ final class SiteHeaderFactory
                             2 => $this->packages->getUrl('assets/patterns/img/patterns/molecules/nav-primary-menu-ic_2x.png'),
                             1 => $this->packages->getUrl('assets/patterns/img/patterns/molecules/nav-primary-menu-ic_1x.png'),
                         ],
-                        'Menu icon'
+                        ''
                     )
                 ),
-                true,
+                false,
                 false,
                 'menu'
             ),
             NavLinkedItem::asLink(new Link('Home', $this->urlGenerator->generate('home'))),
             NavLinkedItem::asLink(new Link('Magazine', $this->urlGenerator->generate('magazine'))),
+            NavLinkedItem::asLink(new Link('Innovation', $this->urlGenerator->generate('labs'))),
             $searchItem,
         ]);
 
         $secondaryLinks = [
             NavLinkedItem::asLink(new Link('About', $this->urlGenerator->generate('about'))),
-            NavLinkedItem::asLink(new Link('Labs', $this->urlGenerator->generate('labs'))),
             NavLinkedItem::asLink(new Link('Community', $this->urlGenerator->generate('community'))),
             NavLinkedItem::asButton(
                 Button::link('Submit my research', 'http://submit.elifesciences.org/', Button::SIZE_EXTRA_SMALL)
