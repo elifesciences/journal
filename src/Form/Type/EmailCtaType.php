7cb3e6bacac506209d71865d81efb1e01603d965
Chris Wilkinson <chriswilkinson84@gmail.com>
2018-06-27 10:02:05 +0100

    ELPP-3415 Only apply CSRF protection if logged in (#1010)
    * Only apply CSRF protection if logged in
    
    * This works
    
    * Clearer purpose

--- src/Form/Type/EmailCtaType.php
+++ src/Form/Type/EmailCtaType.php
@@ -6,7 +6,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -26,12 +25,5 @@ final class EmailCtaType extends AbstractType
                 ]
             )
             ->add('submit', SubmitType::class, ['label' => 'Sign up']);
-    }
-
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'csrf_protection' => false,
-        ]);
     }
 }
