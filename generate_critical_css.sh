658e10928cdabc5b9f3d10b876d5d264b882e9a6
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-07-12 21:33:45 +0100

    Run check in the container

--- generate_critical_css.sh
+++ generate_critical_css.sh
@@ -30,11 +30,11 @@ echo "Generating critical CSS..."
 docker-compose -f docker-compose.yml -f docker-compose.critical-css.yml exec -T critical_css node_modules/.bin/gulp critical-css:generate
 echo "Done"
 
+echo "Checking critical CSS..."
+docker-compose -f docker-compose.yml -f docker-compose.critical-css.yml exec -T critical_css ./check_critical_css.sh
+echo "Done"
+
 echo "Copying critical CSS..."
 rm ${temporary_css}
 docker cp journal_critical_css_1:build/critical-css/. build/critical-css/
-echo "Done"
-
-echo "Checking critical CSS..."
-./check_critical_css.sh
 echo "Done"
