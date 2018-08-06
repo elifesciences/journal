72e45874fca1f7e38f4384e38a57ffab83b57181
Nathan Lisgo <nathan@aeopus.com>
2018-08-06 09:24:27 +0100

    Support YouTube with caption (#1036)
    * Support YouTube with caption
    
    * Update api-sdk
    
    * Fix test

--- test/ViewModel/Converter/Block/YouTubeConverterTest.php
+++ test/ViewModel/Converter/Block/YouTubeConverterTest.php
@@ -4,19 +4,24 @@ namespace test\eLife\Journal\ViewModel\Converter\Block;
 
 use eLife\ApiSdk\Model\Block;
 use eLife\Journal\ViewModel\Converter\Block\YouTubeConverter;
+use eLife\Journal\ViewModel\Converter\ViewModelConverter;
+use eLife\Patterns\PatternRenderer;
 use eLife\Patterns\ViewModel;
 
 final class YouTubeConverterTest extends BlockConverterTestCase
 {
     protected $blockClass = Block\YouTube::class;
-    protected $viewModelClasses = [ViewModel\IFrame::class];
+    protected $viewModelClasses = [ViewModel\IFrame::class, ViewModel\CaptionedAsset::class];
 
     /**
      * @before
      */
     public function setUpConverter()
     {
-        $this->converter = new YouTubeConverter();
+        $this->converter = new YouTubeConverter(
+            $this->createMock(ViewModelConverter::class),
+            $this->createMock(PatternRenderer::class)
+        );
     }
 
     protected function unsupportedModelData() : array
