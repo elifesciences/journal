939149bca5a845fc27bb9c72d4694f3ec7fed668
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-10-04 09:38:39 +0100

    Update eLife's address

--- features/bootstrap/ContactContext.php
+++ features/bootstrap/ContactContext.php
@@ -46,7 +46,7 @@ final class ContactContext extends Context
         $this->assertEmailSent(['do_not_reply@elifesciences.org' => null], ['foo@example.com' => 'Foo Bar'],
             'Question to eLife', 'Thanks for your question. We will respond as soon as we can.
 
-eLife Sciences Publications, Ltd is a limited liability non-profit non-stock corporation incorporated in the State of Delaware, USA, with company number 5030732, and is registered in the UK with company number FC030576 and branch number BR015634 at the address First Floor, 24 Hills Road, Cambridge CB2 1JP.');
+eLife Sciences Publications, Ltd is a limited liability non-profit non-stock corporation incorporated in the State of Delaware, USA, with company number 5030732, and is registered in the UK with company number FC030576 and branch number BR015634 at the address Westbrook Centre, Milton Road, Cambridge CB4 1YG.');
     }
 
     /**
