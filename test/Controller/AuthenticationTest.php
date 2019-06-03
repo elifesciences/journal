6d29bb4498c81c7bbd11967b6ad1abc1fc2d5f23
Giorgio Sironi <g.sironi@elifesciences.org>
2019-06-03 09:32:04 +0100

    Specify / as the redirect path

--- test/Controller/AuthenticationTest.php
+++ test/Controller/AuthenticationTest.php
@@ -308,7 +308,7 @@ final class AuthenticationTest extends WebTestCase
         $crawler = $client->click($crawler->filter('a:contains("Josiah Carberry")')->link());
         $crawler = $client->click($crawler->filter('a:contains("Log out")')->link());
         
-        $this->assertEquals('http://localhost', $crawler->getUri());
+        $this->assertEquals('http://localhost/', $crawler->getUri());
         $this->assertCount(1, $crawler->filter('a:contains("Log in/Register (via ORCID - An ORCID is a persistent digital identifier for researchers)")'));
         $this->assertEmpty($client->getCookieJar()->all());
     }
