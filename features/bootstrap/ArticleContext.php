ac0cbaf113e26fa662b302bb03c40eb600573d54
Chris Wilkinson <c.wilkinson@elifesciences.org>
2019-08-21 09:17:23 +0100

    Support optional component DOIs

--- features/bootstrap/ArticleContext.php
+++ features/bootstrap/ArticleContext.php
@@ -14,11 +14,11 @@ final class ArticleContext extends Context
             new Request(
                 'GET',
                 'http://api.elifesciences.org/articles/00001',
-                ['Accept' => 'application/vnd.elife.article-poa+json; version=2, application/vnd.elife.article-vor+json; version=2']
+                ['Accept' => 'application/vnd.elife.article-poa+json; version=2, application/vnd.elife.article-vor+json; version=3']
             ),
             new Response(
                 200,
-                ['Content-Type' => 'application/vnd.elife.article-vor+json; version=2'],
+                ['Content-Type' => 'application/vnd.elife.article-vor+json; version=3'],
                 json_encode([
                     'status' => 'vor',
                     'stage' => 'published',
--- features/bootstrap/SearchContext.php
+++ features/bootstrap/SearchContext.php
@@ -215,7 +215,7 @@ final class SearchContext extends Context
             new Request(
                 'GET',
                 'http://api.elifesciences.org/articles/00001',
-                ['Accept' => 'application/vnd.elife.article-poa+json; version=2, application/vnd.elife.article-vor+json; version=2']
+                ['Accept' => 'application/vnd.elife.article-poa+json; version=2, application/vnd.elife.article-vor+json; version=3']
             ),
             new Response(
                 200,
