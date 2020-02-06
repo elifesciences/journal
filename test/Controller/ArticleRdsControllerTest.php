60420d9ad5d87b0d3f3e9014a7d907cf7b5228f3
Chris Wilkinson <c.wilkinson@elifesciences.org>
2020-02-06 09:08:23 +0000

    Fix indentation

--- test/Controller/ArticleRdsControllerTest.php
+++ test/Controller/ArticleRdsControllerTest.php
@@ -63,48 +63,48 @@ final class ArticleRdsControllerTest extends WebTestCase
     private function mockArticle($articleId = '00001') : void
     {
         $this->mockApiResponse(
-      new Request(
-        'GET',
-        "http://api.elifesciences.org/articles/{$articleId}",
-        ['Accept' => 'application/vnd.elife.article-poa+json; version=2, application/vnd.elife.article-vor+json; version=3']
-      ),
-      new Response(
-        200,
-        ['Content-Type' => 'application/vnd.elife.article-vor+json; version=3'],
-        json_encode([
-          'status' => 'vor',
-          'stage' => 'published',
-          'id' => $articleId,
-          'version' => 3,
-          'type' => 'research-article',
-          'doi' => "10.7554/eLife.{$articleId}",
-          'title' => 'Article title',
-          'published' => '2010-01-01T00:00:00Z',
-          'versionDate' => '2012-01-01T00:00:00Z',
-          'statusDate' => '2011-01-01T00:00:00Z',
-          'volume' => 1,
-          'elocationId' => "e{$articleId}",
-          'xml' => 'http://www.example.com/xml',
-          'copyright' => [
-            'license' => 'CC-BY-4.0',
-            'holder' => 'Bar',
-            'statement' => 'Copyright statement.',
-          ],
-          'body' => [
-            [
-              'type' => 'section',
-              'id' => 's-1',
-              'title' => 'Section',
-              'content' => [
-                [
-                  'type' => 'paragraph',
-                  'text' => 'Text.',
-                ],
-              ],
-            ],
-          ],
-        ])
-      )
-    );
+            new Request(
+                'GET',
+                "http://api.elifesciences.org/articles/{$articleId}",
+                ['Accept' => 'application/vnd.elife.article-poa+json; version=2, application/vnd.elife.article-vor+json; version=3']
+            ),
+            new Response(
+                200,
+                ['Content-Type' => 'application/vnd.elife.article-vor+json; version=3'],
+                json_encode([
+                    'status' => 'vor',
+                    'stage' => 'published',
+                    'id' => $articleId,
+                    'version' => 3,
+                    'type' => 'research-article',
+                    'doi' => "10.7554/eLife.{$articleId}",
+                    'title' => 'Article title',
+                    'published' => '2010-01-01T00:00:00Z',
+                    'versionDate' => '2012-01-01T00:00:00Z',
+                    'statusDate' => '2011-01-01T00:00:00Z',
+                    'volume' => 1,
+                    'elocationId' => "e{$articleId}",
+                    'xml' => 'http://www.example.com/xml',
+                    'copyright' => [
+                        'license' => 'CC-BY-4.0',
+                        'holder' => 'Bar',
+                        'statement' => 'Copyright statement.',
+                    ],
+                    'body' => [
+                        [
+                            'type' => 'section',
+                            'id' => 's-1',
+                            'title' => 'Section',
+                            'content' => [
+                                [
+                                    'type' => 'paragraph',
+                                    'text' => 'Text.',
+                                ],
+                            ],
+                        ],
+                    ],
+                ])
+            )
+        );
     }
 }
