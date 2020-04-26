d0dd5e035b21d3cc588d52e3cf2bdeb845494170
nlisgo <nathan@aeopus.com>
2020-04-26 19:38:09 +0100

    Introduce google map block

---
+++ src/ViewModel/Converter/Block/GoogleMapConverter.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace eLife\Journal\ViewModel\Converter\Block;
+
+use eLife\ApiSdk\Model\Block;
+use eLife\Journal\ViewModel\Converter\ViewModelConverter;
+use eLife\Patterns\PatternRenderer;
+use eLife\Patterns\ViewModel;
+
+final class GoogleMapConverter implements ViewModelConverter
+{
+    use CreatesCaptionedAsset;
+
+    private $viewModelConverter;
+    private $patternRenderer;
+
+    public function __construct(ViewModelConverter $viewModelConverter, PatternRenderer $patternRenderer)
+    {
+        $this->viewModelConverter = $viewModelConverter;
+        $this->patternRenderer = $patternRenderer;
+    }
+
+    /**
+     * @param Block\GoogleMap $object
+     */
+    public function convert($object, string $viewModel = null, array $context = []) : ViewModel
+    {
+        $iframe = new ViewModel\IFrame('https://www.google.com/maps/d/embed?mid='.$object->getId(), $object->getWidth(), $object->getHeight(), $object->getTitle());
+
+        if (!$object->getTitle() && $object->getCaption()->isEmpty()) {
+            return $iframe;
+        }
+
+        return $this->createCaptionedAsset($iframe, $object);
+    }
+
+    public function supports($object, string $viewModel = null, array $context = []) : bool
+    {
+        return $object instanceof Block\GoogleMap;
+    }
+
+    protected function getViewModelConverter() : ViewModelConverter
+    {
+        return $this->viewModelConverter;
+    }
+
+    protected function getPatternRenderer() : PatternRenderer
+    {
+        return $this->patternRenderer;
+    }
+}
