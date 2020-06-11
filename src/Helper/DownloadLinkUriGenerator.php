ab5148fbffac9cde24d592c6e5ac7f04c0213b86
nlisgo <nathan@aeopus.com>
2020-06-11 20:14:16 +0100

    fix rel canonical

--- src/Helper/DownloadLinkUriGenerator.php
+++ src/Helper/DownloadLinkUriGenerator.php
@@ -10,7 +10,7 @@ final class DownloadLinkUriGenerator
 {
     const URL_UNSAFE_CHARS = '+/=';
     const URL_UNSAFE_REPLACES = '._-';
-    const WRAP_REL_CANONICAL = '<relCanonical>';
+    const WRAP_REL_CANONICAL = '%relCanonical%';
 
     private $urlGenerator;
     private $uriSigner;
@@ -45,6 +45,6 @@ final class DownloadLinkUriGenerator
         $name = array_pop($path);
         list($uri, $relCanonical) = explode(self::WRAP_REL_CANONICAL, base64_decode(strtr(array_pop($path), self::URL_UNSAFE_REPLACES, self::URL_UNSAFE_CHARS)).self::WRAP_REL_CANONICAL);
 
-        return new DownloadLink(base64_decode(strtr($uri, self::URL_UNSAFE_REPLACES, self::URL_UNSAFE_CHARS)), $name, $relCanonical ?? null);
+        return new DownloadLink($uri, $name, $relCanonical ?? null);
     }
 }
