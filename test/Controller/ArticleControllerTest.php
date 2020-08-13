3690afd3ed9f7afbc4a3ae8244744bc335cfc156
Nathan Lisgo <nathan@aeopus.com>
2020-08-13 16:50:03 +0100

    Add .zip extension to era archive download (#1320)

--- test/Controller/ArticleControllerTest.php
+++ test/Controller/ArticleControllerTest.php
@@ -1472,7 +1472,7 @@ final class ArticleControllerTest extends PageTestCase
         );
         $this->assertNotEmpty($crawler->filter('.article-download-links-list__link')->selectLink('Executable version'));
         $this->assertNotEmpty($crawler->filter('.article-download-links-list__secondary_link')->selectLink('What are executable versions?'));
-        $this->assertContains('/elife-id-of-article-with-rds-v3-era.dar', $crawler->filter('.article-download-links-list__link')->selectLink('Executable version')->attr('href'));
+        $this->assertContains('/elife-id-of-article-with-rds-v3-era.zip', $crawler->filter('.article-download-links-list__link')->selectLink('Executable version')->attr('href'));
         $this->assertContains('Executable code', $crawler->filter('.view-selector')->text());
     }
 
