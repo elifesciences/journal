d1f400137807b64628cababc39df7862c52971a2
Nathan Lisgo <nathan@aeopus.com>
2020-11-26 10:38:14 +0000

    Digest schema image (#1400)

--- test/Controller/DigestControllerTest.php
+++ test/Controller/DigestControllerTest.php
@@ -4,6 +4,9 @@ namespace test\eLife\Journal\Controller;
 
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Response;
+use ML\JsonLD\JsonLD;
+use ML\JsonLD\RdfConstants;
+use ML\JsonLD\TypedValue;
 use test\eLife\Journal\Providers;
 
 final class DigestControllerTest extends PageTestCase
@@ -143,6 +146,32 @@ final class DigestControllerTest extends PageTestCase
         $client->request('GET', $url);
 
         $this->assertTrue($client->getResponse()->isRedirect($expectedUrl));
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_schema_org_metadata()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', $this->getUrl());
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+
+        $script = $crawler->filter('script[type="application/ld+json"]');
+        $this->assertNotEmpty($script);
+
+        $value = $script->text();
+        $this->assertJson($value);
+
+        $this->markTestIncomplete('This test fails if schema.org is broken!');
+
+        $graph = JsonLD::getDocument($value)->getGraph();
+        $node = $graph->getNodes()[0];
+
+        $this->assertEquals('http://schema.org/NewsArticle', $node->getType()->getId());
+        $this->assertEquals(new TypedValue('Digest title', RdfConstants::XSD_STRING), $node->getProperty('http://schema.org/headline'));
     }
 
     /**
