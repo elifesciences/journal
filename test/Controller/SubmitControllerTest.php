1388df1e41f89c49b2f6674ea441d35ffe354dd7
nlisgo <nathan@aeopus.com>
2020-11-30 10:06:56 +0000

    Remove test for now

--- test/Controller/SubmitControllerTest.php
+++ test/Controller/SubmitControllerTest.php
@@ -14,44 +14,6 @@ final class SubmitControllerTest extends WebTestCase
 {
     /**
      * @test
-     */
-    public function it_requires_you_to_be_logged_in()
-    {
-        $client = static::createClient();
-
-        $client->request('GET', '/submit?return_url='.urlencode('http://foo.elifesciences.org/path'));
-        $response = $client->getResponse();
-
-        $this->assertTrue($response->isRedirect());
-        $location = Uri::withoutQueryValue(new Uri($response->headers->get('Location')), 'state');
-        $this->assertSameUri('http://api.elifesciences.org/oauth2/authorize?response_type=code&client_id=journal_client_id&redirect_uri=http%3A%2F%2Flocalhost%2Flog-in%2Fcheck', $location);
-
-        $state = parse_query((new Uri($response->headers->get('Location')))->getQuery())['state'];
-
-        $this->readyToken();
-
-        $client->request('GET', "/log-in/check?code=foo&state={$state}");
-        $response = $client->getResponse();
-
-        $this->assertTrue($response->isRedirect());
-        $this->assertSameUri('http://localhost/submit?return_url=http%3A%2F%2Ffoo.elifesciences.org%2Fpath', $response->headers->get('Location'));
-
-        $client->followRedirect();
-        $response = $client->getResponse();
-
-        $this->assertTrue($response->isRedirect());
-        $location = new Uri($response->headers->get('Location'));
-        $token = parse_query($location->getQuery())['token'];
-
-        $this->assertSameUri('http://foo.elifesciences.org/path', $location->withQuery(''));
-
-        $jwt = (array) JWT::decode($token, $this->getParameter('submission_client_secret'), ['HS256']);
-
-        $this->assertTrue($jwt['new-session']);
-    }
-
-    /**
-     * @test
      * @dataProvider invalidDomainProvider
      */
     public function it_does_not_redirect_if_return_url_is_not_trusted_with_invalid_domain($domain)
