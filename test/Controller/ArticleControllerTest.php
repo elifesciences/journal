b6d9611cb22644aae4098d21ca32e89da85d5e76
Dave Price <d.price@elifesciences.org>
2021-04-14 11:54:06 +0100

    Add datasets tests

--- test/Controller/ArticleControllerTest.php
+++ test/Controller/ArticleControllerTest.php
@@ -32,167 +32,6 @@ final class ArticleControllerTest extends PageTestCase
     /**
      * @test
      */
-    public function it_may_have_a_data_availability_statement()
-    {
-        $client = static::createClient();
-
-        $this->mockApiResponse(
-            new Request(
-                'GET',
-                'http://api.elifesciences.org/articles/00001',
-                ['Accept' => 'application/vnd.elife.article-poa+json; version=3, application/vnd.elife.article-vor+json; version=5']
-            ),
-            new Response(
-                200,
-                ['Content-Type' => 'application/vnd.elife.article-vor+json; version=5'],
-                json_encode([
-                    'status' => 'vor',
-                    'stage' => 'published',
-                    'id' => '00001',
-                    'version' => 1,
-                    'type' => 'research-article',
-                    'doi' => '10.7554/eLife.00001',
-                    'title' => 'Article 1 title',
-                    'published' => '2010-01-01T00:00:00Z',
-                    'versionDate' => '2010-01-01T00:00:00Z',
-                    'statusDate' => '2010-01-01T00:00:00Z',
-                    'volume' => 1,
-                    'elocationId' => 'e00001',
-                    'copyright' => [
-                        'license' => 'CC-BY-4.0',
-                        'holder' => 'Bar',
-                        'statement' => 'Copyright statement.',
-                    ],
-                    'authorLine' => 'Foo Bar',
-                    'authors' => [
-                        [
-                            'type' => 'person',
-                            'name' => [
-                                'preferred' => 'Foo Bar',
-                                'index' => 'Bar, Foo',
-                            ],
-                        ],
-                    ],
-                    'dataSets' => [
-                        'availability' => [
-                            [
-                                'type' => 'paragraph',
-                                'text' => 'Data availability statement',
-                            ],
-                        ],
-                        'generated' => [
-                            [
-                                'id' => 'dataro1',
-                                'authors' => [
-                                    [
-                                        'type' => 'person',
-                                        'name' => [
-                                            'preferred' => 'Foo Bar',
-                                            'index' => 'Bar, Foo',
-                                        ],
-                                    ],
-                                    [
-                                        'type' => 'group',
-                                        'name' => 'Baz',
-                                    ],
-                                ],
-                                'date' => '2013',
-                                'title' => 'Data set 1',
-                                'dataId' => 'DataSet1',
-                                'uri' => 'http://www.example.com/',
-                                'details' => 'Data set details.',
-                            ],
-                        ],
-                        'used' => [
-                            [
-                                'id' => 'dataro2',
-                                'authors' => [
-                                    [
-                                        'type' => 'person',
-                                        'name' => [
-                                            'preferred' => 'Foo Bar',
-                                            'index' => 'Bar, Foo',
-                                        ],
-                                    ],
-                                ],
-                                'date' => '2014',
-                                'title' => 'Data set 2',
-                            ],
-                        ],
-                    ],
-                    'body' => [
-                        [
-                            'type' => 'section',
-                            'id' => 's-1',
-                            'title' => 'Introduction',
-                            'content' => [
-                                [
-                                    'type' => 'paragraph',
-                                    'text' => 'Fossil hominins were first recognized in the Dinaledi Chamber in the Rising Star cave system in October 2013. During a relatively short excavation, our team recovered an extensive collection of 1550 hominin specimens, representing nearly every element of the skeleton multiple times (Figure 1), including many complete elements and morphologically informative fragments, some in articulation, as well as smaller fragments many of which could be refit into more complete elements. The collection is a morphologically homogeneous sample that can be attributed to no previously-known hominin species. Here we describe this new species, <i>Homo naledi</i>. We have not defined <i>H. naledi</i> narrowly based on a single jaw or skull because the entire body of material has informed our understanding of its biology.',
-                                ],
-                            ],
-                        ],
-                    ],
-                ])
-            )
-        );
-
-        $this->mockApiResponse(
-            new Request(
-                'GET',
-                'http://api.elifesciences.org/articles/00001/versions',
-                [
-                    'Accept' => [
-                        'application/vnd.elife.article-history+json; version=1',
-                    ],
-                ]
-            ),
-            new Response(
-                200,
-                ['Content-Type' => 'application/vnd.elife.article-history+json; version=1'],
-                json_encode([
-                    'versions' => [
-                        [
-                            'status' => 'vor',
-                            'stage' => 'published',
-                            'id' => '00001',
-                            'version' => 1,
-                            'type' => 'research-article',
-                            'doi' => '10.7554/eLife.00001',
-                            'title' => 'Article 1 title',
-                            'published' => '2010-01-01T00:00:00Z',
-                            'versionDate' => '2010-01-01T00:00:00Z',
-                            'statusDate' => '2010-01-01T00:00:00Z',
-                            'volume' => 1,
-                            'elocationId' => 'e00001',
-                            'copyright' => [
-                                'license' => 'CC-BY-4.0',
-                                'holder' => 'Bar',
-                                'statement' => 'Copyright statement.',
-                            ],
-                            'authorLine' => 'Foo Bar',
-                        ],
-                    ],
-                ])
-            )
-        );
-
-        $crawler = $client->request('GET', '/articles/00001');
-
-        $dataAvailability = $crawler->filter('.grid-column > section:nth-of-type(2)');
-        $this->assertSame('Data availability',
-            $dataAvailability->filter('header > h2')->text());
-
-        // Check text of data availability statement
-        // Check existence and text of generated datasets
-        // Check existence and text of used datasets
-
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
-    }
-
-    /**
-     * @test
-     */
     public function it_displays_an_preview_article_page()
     {
         $client = static::createClient();
@@ -2103,7 +1942,46 @@ final class ArticleControllerTest extends PageTestCase
                         'availability' => [
                             [
                                 'type' => 'paragraph',
-                                'text' => 'Data availability',
+                                'text' => 'Data availability statement',
+                            ],
+                        ],
+                        'generated' => [
+                            [
+                                'id' => 'dataro1',
+                                'authors' => [
+                                    [
+                                        'type' => 'person',
+                                        'name' => [
+                                            'preferred' => 'Foo Bar',
+                                            'index' => 'Bar, Foo',
+                                        ],
+                                    ],
+                                    [
+                                        'type' => 'group',
+                                        'name' => 'Baz',
+                                    ],
+                                ],
+                                'date' => '2013',
+                                'title' => 'Data set 1',
+                                'dataId' => 'DataSet1',
+                                'uri' => 'http://www.example.com/',
+                                'details' => 'Data set details.',
+                            ],
+                        ],
+                        'used' => [
+                            [
+                                'id' => 'dataro2',
+                                'authors' => [
+                                    [
+                                        'type' => 'person',
+                                        'name' => [
+                                            'preferred' => 'Foo Bar',
+                                            'index' => 'Bar, Foo',
+                                        ],
+                                    ],
+                                ],
+                                'date' => '2014',
+                                'title' => 'Data set 2',
                             ],
                         ],
                     ],
@@ -2588,6 +2466,12 @@ final class ArticleControllerTest extends PageTestCase
         $dataAvailability = $crawler->filter('.grid-column > section:nth-of-type(5)');
         $this->assertSame('Data availability',
             $dataAvailability->filter('header > h2')->text());
+        $data = $dataAvailability->filter('.article-section__body')->children();
+        $this->assertSame('Data availability statement', trim($data->eq(0)->text()));
+        $this->assertSame('The following data sets were generated', trim($data->eq(1)->text()));
+        $this->assertSame('Data set 1', trim($data->eq(2)->filter('.reference__title')->text()));
+        $this->assertSame('The following previously published data sets were used', trim($data->eq(3)->text()));
+        $this->assertSame('Data set 2', trim($data->eq(4)->filter('.reference__title')->text()));
         $references = $crawler->filter('.grid-column > section:nth-of-type(6)');
         $this->assertSame('References',
             $references->filter('header > h2')->text());
@@ -2765,6 +2649,170 @@ final class ArticleControllerTest extends PageTestCase
             ],
             array_map('trim', $crawler->filter('.view-selector__jump_link_item')->extract('_text'))
         );
+    }
+
+    /**
+     * @test
+     */
+    public function it_may_have_a_data_availability_statement()
+    {
+        $client = static::createClient();
+
+        $this->mockApiResponse(
+            new Request(
+                'GET',
+                'http://api.elifesciences.org/articles/00001',
+                ['Accept' => 'application/vnd.elife.article-poa+json; version=3, application/vnd.elife.article-vor+json; version=5']
+            ),
+            new Response(
+                200,
+                ['Content-Type' => 'application/vnd.elife.article-vor+json; version=5'],
+                json_encode([
+                    'status' => 'vor',
+                    'stage' => 'published',
+                    'id' => '00001',
+                    'version' => 1,
+                    'type' => 'research-article',
+                    'doi' => '10.7554/eLife.00001',
+                    'title' => 'Article 1 title',
+                    'published' => '2010-01-01T00:00:00Z',
+                    'versionDate' => '2010-01-01T00:00:00Z',
+                    'statusDate' => '2010-01-01T00:00:00Z',
+                    'volume' => 1,
+                    'elocationId' => 'e00001',
+                    'copyright' => [
+                        'license' => 'CC-BY-4.0',
+                        'holder' => 'Bar',
+                        'statement' => 'Copyright statement.',
+                    ],
+                    'authorLine' => 'Foo Bar',
+                    'authors' => [
+                        [
+                            'type' => 'person',
+                            'name' => [
+                                'preferred' => 'Foo Bar',
+                                'index' => 'Bar, Foo',
+                            ],
+                        ],
+                    ],
+                    'dataSets' => [
+                        'availability' => [
+                            [
+                                'type' => 'paragraph',
+                                'text' => 'Data availability statement',
+                            ],
+                        ],
+                        'generated' => [
+                            [
+                                'id' => 'dataro1',
+                                'authors' => [
+                                    [
+                                        'type' => 'person',
+                                        'name' => [
+                                            'preferred' => 'Foo Bar',
+                                            'index' => 'Bar, Foo',
+                                        ],
+                                    ],
+                                    [
+                                        'type' => 'group',
+                                        'name' => 'Baz',
+                                    ],
+                                ],
+                                'date' => '2013',
+                                'title' => 'Data set 1',
+                                'dataId' => 'DataSet1',
+                                'uri' => 'http://www.example.com/',
+                                'details' => 'Data set details.',
+                            ],
+                        ],
+                        'used' => [
+                            [
+                                'id' => 'dataro2',
+                                'authors' => [
+                                    [
+                                        'type' => 'person',
+                                        'name' => [
+                                            'preferred' => 'Foo Bar',
+                                            'index' => 'Bar, Foo',
+                                        ],
+                                    ],
+                                ],
+                                'date' => '2014',
+                                'title' => 'Data set 2',
+                            ],
+                        ],
+                    ],
+                    'body' => [
+                        [
+                            'type' => 'section',
+                            'id' => 's-1',
+                            'title' => 'Introduction',
+                            'content' => [
+                                [
+                                    'type' => 'paragraph',
+                                    'text' => 'Fossil hominins were first recognized in the Dinaledi Chamber in the Rising Star cave system in October 2013. During a relatively short excavation, our team recovered an extensive collection of 1550 hominin specimens, representing nearly every element of the skeleton multiple times (Figure 1), including many complete elements and morphologically informative fragments, some in articulation, as well as smaller fragments many of which could be refit into more complete elements. The collection is a morphologically homogeneous sample that can be attributed to no previously-known hominin species. Here we describe this new species, <i>Homo naledi</i>. We have not defined <i>H. naledi</i> narrowly based on a single jaw or skull because the entire body of material has informed our understanding of its biology.',
+                                ],
+                            ],
+                        ],
+                    ],
+                ])
+            )
+        );
+
+        $this->mockApiResponse(
+            new Request(
+                'GET',
+                'http://api.elifesciences.org/articles/00001/versions',
+                [
+                    'Accept' => [
+                        'application/vnd.elife.article-history+json; version=1',
+                    ],
+                ]
+            ),
+            new Response(
+                200,
+                ['Content-Type' => 'application/vnd.elife.article-history+json; version=1'],
+                json_encode([
+                    'versions' => [
+                        [
+                            'status' => 'vor',
+                            'stage' => 'published',
+                            'id' => '00001',
+                            'version' => 1,
+                            'type' => 'research-article',
+                            'doi' => '10.7554/eLife.00001',
+                            'title' => 'Article 1 title',
+                            'published' => '2010-01-01T00:00:00Z',
+                            'versionDate' => '2010-01-01T00:00:00Z',
+                            'statusDate' => '2010-01-01T00:00:00Z',
+                            'volume' => 1,
+                            'elocationId' => 'e00001',
+                            'copyright' => [
+                                'license' => 'CC-BY-4.0',
+                                'holder' => 'Bar',
+                                'statement' => 'Copyright statement.',
+                            ],
+                            'authorLine' => 'Foo Bar',
+                        ],
+                    ],
+                ])
+            )
+        );
+
+        $crawler = $client->request('GET', '/articles/00001');
+
+        $dataAvailability = $crawler->filter('.grid-column > section:nth-of-type(2)');
+        $this->assertSame('Data availability',
+            $dataAvailability->filter('header > h2')->text());
+
+        $data = $dataAvailability->filter('.article-section__body')->children();
+        $this->assertSame('Data availability statement', trim($data->eq(0)->text()));
+        $this->assertSame('The following data sets were generated', trim($data->eq(1)->text()));
+        $this->assertSame('Data set 1', trim($data->eq(2)->filter('.reference__title')->text()));
+        $this->assertSame('The following previously published data sets were used', trim($data->eq(3)->text()));
+        $this->assertSame('Data set 2', trim($data->eq(4)->filter('.reference__title')->text()));
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
     }
 
     /**
