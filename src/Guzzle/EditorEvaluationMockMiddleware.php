aebd3b63eaaa741d5e27a2e63f4e87d823d429d7
nlisgo <nathan@aeopus.com>
2021-10-05 14:32:04 +0100

    adjust property name of scietyUri

--- src/Guzzle/EditorEvaluationMockMiddleware.php
+++ src/Guzzle/EditorEvaluationMockMiddleware.php
@@ -47,7 +47,7 @@ final class EditorEvaluationMockMiddleware
                         'text' => 'Collagen is a major component of extracellular matrix. The authors have identified a high-affinity inhibitory collagen receptor LAIR-1 and a soluble decoy receptor LAIR-2 (with even higher binding affinity to collagen), which can be therapeutically targeted to block tumor progression. Dr Meyaard and colleagues have also generated a dimeric LAIR-2 human IgG1 Fc fusion protein NC410 for therapeutic use. With humanized mouse models engrafted with functional human immune systems (PBMC), they have explored the anti-cancer efficacy of NC410 and revealed its impact on modulating immune responses. Furthermore, they extended this study to identify biomarkers of predictive value for NC410-based anti-cancer therapy.',
                     ],
                 ],
-                'uri' => 'https://sciety.org/articles/activity/10.1101/2020.11.21.391326',
+                'scietyUri' => 'https://sciety.org/articles/activity/10.1101/2020.11.21.391326',
             ];
         }
 
