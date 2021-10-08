be3699d3b249f169bbc0f5b5a3de1cf28344bb49
nlisgo <nathan@aeopus.com>
2021-10-08 09:17:57 +0100

    Revert "Introduce editor's evaluation (#1525)"
    This reverts commit 657d1c43c9a3dfa0f5082784314348a9f346a098.

--- src/Guzzle/SubjectRewritingMiddleware.php
+++ src/Guzzle/SubjectRewritingMiddleware.php
@@ -101,8 +101,8 @@ final class SubjectRewritingMiddleware
 
                     case 'application/vnd.elife.article-poa+json; version=2':
                     case 'application/vnd.elife.article-poa+json; version=3':
+                    case 'application/vnd.elife.article-vor+json; version=4':
                     case 'application/vnd.elife.article-vor+json; version=5':
-                    case 'application/vnd.elife.article-vor+json; version=6':
                     case 'application/vnd.elife.blog-article+json; version=2':
                         $data = $this->updateItem($data);
                         break;
