aeab35e7b24adca1c184e903319298b83697595e
nlisgo <nathan@aeopus.com>
2021-10-14 21:33:34 +0100

    Introduce setUp

--- test/Twig/JsonLdSchemaOrgExtensionTest.php
+++ test/Twig/JsonLdSchemaOrgExtensionTest.php
@@ -20,17 +20,24 @@ use function GuzzleHttp\Promise\promise_for;
 
 final class JsonLdSchemaOrgExtensionTest extends TestCase
 {
+    private $urlGenerator;
+    private $packages;
+    private $extension;
+
+    public function setUp()
+    {
+        $this->urlGenerator = $this->createMock(UrlGeneratorInterface::class);
+        $this->packages = $this->createMock(Packages::class);
+
+        $this->extension = new JsonLdSchemaOrgExtension($this->urlGenerator, $this->packages);
+    }
+
     /**
      * @test
      */
     public function it_is_a_twig_extension()
     {
-        $urlGenerator = $this->createMock(UrlGeneratorInterface::class);
-        $packages = $this->createMock(Packages::class);
-
-        $extension = new JsonLdSchemaOrgExtension($urlGenerator, $packages);
-
-        $this->assertInstanceOf(Twig_ExtensionInterface::class, $extension);
+        $this->assertInstanceOf(Twig_ExtensionInterface::class, $this->extension);
     }
 
     /**
@@ -38,15 +45,10 @@ final class JsonLdSchemaOrgExtensionTest extends TestCase
      */
     public function it_must_receive_a_content_model()
     {
-        $urlGenerator = $this->createMock(UrlGeneratorInterface::class);
-        $packages = $this->createMock(Packages::class);
-
-        $urlGenerator->expects($this->once())->method('getContext')->willReturn(new RequestContext());
-
-        $extension = new JsonLdSchemaOrgExtension($urlGenerator, $packages);
+        $this->urlGenerator->expects($this->once())->method('getContext')->willReturn(new RequestContext());
 
         $file = new File('image/jpeg', 'https://iiif.elifesciences.org/example.jpg/full/full/0/default.jpg', 'example.jpg');
-        $extension->generate(new Digest(
+        $this->extension->generate(new Digest(
             'id',
             'Digest title',
             null,
@@ -61,7 +63,7 @@ final class JsonLdSchemaOrgExtensionTest extends TestCase
         ));
 
         $this->expectException(TypeError::class);
-        $extension->generate('not content model');
+        $this->extension->generate('not content model');
     }
 
     /**
@@ -69,18 +71,12 @@ final class JsonLdSchemaOrgExtensionTest extends TestCase
      */
     public function it_will_generate_json_ld_schema_from_digest()
     {
-
-        $urlGenerator = $this->createMock(UrlGeneratorInterface::class);
-        $packages = $this->createMock(Packages::class);
-
-        $urlGenerator->expects($this->once())->method('getContext')->willReturn(new RequestContext(null, 'GET', 'journal', 'https'));
-        $packages->expects($this->once())->method('getUrl')->willReturn('/assets/patterns/img/patterns/organisms/elife-logo-symbol@2x.png');
-
-        $extension = new JsonLdSchemaOrgExtension($urlGenerator, $packages);
+        $this->urlGenerator->expects($this->once())->method('getContext')->willReturn(new RequestContext(null, 'GET', 'journal', 'https'));
+        $this->packages->expects($this->once())->method('getUrl')->willReturn('/assets/patterns/img/patterns/organisms/elife-logo-symbol@2x.png');
 
         $file = new File('image/jpeg', 'https://iiif.elifesciences.org/example.jpg/full/full/0/default.jpg', 'example.jpg');
         $thumbnail = $subjectBanner = $subjectThumbnail = new Image('', 'https://iiif.elifesciences.org/example.jpg', new EmptySequence(), $file, 1000, 500, 50, 50);
-        $json = $extension->generateJson(new Digest(
+        $json = $this->extension->generateJson(new Digest(
             'id',
             'Digest title',
             'Impact statement',
@@ -124,15 +120,5 @@ final class JsonLdSchemaOrgExtensionTest extends TestCase
                 'issn' => '2050-084X',
             ],
         ], $json);
-    }
-
-
-    /**
-     * @test
-     * @depends it_is_a_twig_extension
-     */
-    public function it_turns_model_into_json_ld_schema()
-    {
-        $this->assertTrue(true);
     }
 }
