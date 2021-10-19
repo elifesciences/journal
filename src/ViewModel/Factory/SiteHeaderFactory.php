2da3dc2343e7223b7bfdb7bf667a425014d426f2
Dave Price <d.price@elifesciences.org>
2021-10-19 14:56:04 +0100

    Refactor for Newsletter link

--- src/ViewModel/Factory/SiteHeaderFactory.php
+++ src/ViewModel/Factory/SiteHeaderFactory.php
@@ -126,7 +126,7 @@ final class SiteHeaderFactory
         }
 
         $secondaryLinks = [
-            NavLinkedItem::asLink(new Link('Newsletter', $this->packages->getUrl('https://crm.elifesciences.org/crm/content-alerts'))),
+            NavLinkedItem::asLink(new Link('Newsletter', 'https://crm.elifesciences.org/crm/content-alerts')),
             NavLinkedItem::asLink(new Link('Community', $this->urlGenerator->generate('community'))),
             NavLinkedItem::asButton(
                 Button::link('Submit my research', $submitUrl ?? $this->submitUrl, Button::SIZE_EXTRA_SMALL, Button::STYLE_DEFAULT, true, false, 'submitResearchButton')
