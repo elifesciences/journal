a6ef19043cc45d00250bd517eeb2eee00290b5ff
nlisgo <nathan@aeopus.com>
2021-10-22 11:35:27 +0100

    Add checkSubscription and triggerPreferencesEmail to interface

--- src/Guzzle/CiviCrmClientInterface.php
+++ src/Guzzle/CiviCrmClientInterface.php
@@ -10,4 +10,8 @@ interface CiviCrmClientInterface
     public function __construct(ClientInterface $client, string $apiKey, string $siteKey);
 
     public function subscribe(string $email, array $preferences, string $firstName = null, string $lastName = null) : PromiseInterface;
+
+    public function checkSubscription(string $identifier, $isPreferencesId = false) : PromiseInterface;
+
+    public function triggerPreferencesEmail(int $contactId) : PromiseInterface;
 }
