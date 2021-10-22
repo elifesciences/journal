d0daa46287422d2608d8ff93e0c068eed7ca8b37
nlisgo <nathan@aeopus.com>
2021-10-22 11:59:01 +0100

    Remove constructor

--- src/Guzzle/CiviCrmClientInterface.php
+++ src/Guzzle/CiviCrmClientInterface.php
@@ -2,12 +2,9 @@
 
 namespace eLife\Journal\Guzzle;
 
-use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Promise\PromiseInterface;
 
 interface CiviCrmClientInterface
 {
-    public function __construct(ClientInterface $client, string $apiKey, string $siteKey);
-
     public function subscribe(string $email, array $preferences, string $firstName = null, string $lastName = null) : PromiseInterface;
 }
