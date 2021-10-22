35e383da7aaa0e243b91123c55d36b6a0fbe0246
nlisgo <nathan@aeopus.com>
2021-10-22 11:33:56 +0100

    Add constructor to interface

--- src/Guzzle/CiviCrmClientInterface.php
+++ src/Guzzle/CiviCrmClientInterface.php
@@ -2,9 +2,12 @@
 
 namespace eLife\Journal\Guzzle;
 
+use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Promise\PromiseInterface;
 
 interface CiviCrmClientInterface
 {
+    public function __construct(ClientInterface $client, string $apiKey, string $siteKey);
+
     public function subscribe(string $email, array $preferences, string $firstName = null, string $lastName = null) : PromiseInterface;
 }
