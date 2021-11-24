7b8f1455965ed0a8c8b9bcfd44f66b7ae13fe56c
nlisgo <nathan@aeopus.com>
2021-11-24 13:45:01 +0000

    Ensure we use media type versions in ApiSdk

--- src/Guzzle/StatusDateOverrideMiddleware.php
+++ src/Guzzle/StatusDateOverrideMiddleware.php
@@ -2,7 +2,25 @@
 
 namespace eLife\Journal\Guzzle;
 
+use eLife\ApiClient\ApiClient\ArticlesClient;
+use eLife\ApiClient\ApiClient\CollectionsClient;
+use eLife\ApiClient\ApiClient\CommunityClient;
+use eLife\ApiClient\ApiClient\CoversClient;
+use eLife\ApiClient\ApiClient\HighlightsClient;
+use eLife\ApiClient\ApiClient\PodcastClient;
+use eLife\ApiClient\ApiClient\PressPackagesClient;
+use eLife\ApiClient\ApiClient\RecommendationsClient;
+use eLife\ApiClient\ApiClient\SearchClient;
 use eLife\ApiClient\MediaType;
+use eLife\ApiSdk\Client\Articles;
+use eLife\ApiSdk\Client\Collections;
+use eLife\ApiSdk\Client\Community;
+use eLife\ApiSdk\Client\Covers;
+use eLife\ApiSdk\Client\Highlights;
+use eLife\ApiSdk\Client\PodcastEpisodes;
+use eLife\ApiSdk\Client\PressPackages;
+use eLife\ApiSdk\Client\Recommendations;
+use eLife\ApiSdk\Client\Search;
 use InvalidArgumentException;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -39,35 +57,32 @@ final class StatusDateOverrideMiddleware
                 }
 
                 switch ((string) $mediaType) {
-                    case 'application/vnd.elife.article-history+json; version=2':
+                    case (string) new MediaType(ArticlesClient::TYPE_ARTICLE_HISTORY, Articles::VERSION_ARTICLE_HISTORY):
                         $data['versions'] = $this->updateItems($data['versions']);
                         break;
 
-                    case 'application/vnd.elife.article-list+json; version=1':
-                    case 'application/vnd.elife.community-list+json; version=1':
-                    case 'application/vnd.elife.recommendations+json; version=1':
+                    case (string) new MediaType(ArticlesClient::TYPE_ARTICLE_LIST, Articles::VERSION_ARTICLE_LIST):
+                    case (string) new MediaType(CommunityClient::TYPE_COMMUNITY_LIST, Community::VERSION_COMMUNITY_LIST):
+                    case (string) new MediaType(RecommendationsClient::TYPE_RECOMMENDATIONS, Recommendations::VERSION_RECOMMENDATIONS):
                         $data['items'] = $this->updateItems($data['items']);
                         break;
 
-                    case 'application/vnd.elife.article-related+json; version=1':
+                    case (string) new MediaType(ArticlesClient::TYPE_ARTICLE_RELATED, Articles::VERSION_ARTICLE_RELATED):
                         $data = $this->updateItems($data);
                         break;
 
-                    case 'application/vnd.elife.article-poa+json; version=2':
-                    case 'application/vnd.elife.article-poa+json; version=3':
-                    case 'application/vnd.elife.article-vor+json; version=5':
-                    case 'application/vnd.elife.article-vor+json; version=6':
+                    case (string) new MediaType(ArticlesClient::TYPE_ARTICLE_POA, Articles::VERSION_ARTICLE_POA):
+                    case (string) new MediaType(ArticlesClient::TYPE_ARTICLE_VOR, Articles::VERSION_ARTICLE_VOR):
                         $data = $this->updateItem($data);
                         break;
 
-                    case 'application/vnd.elife.collection+json; version=1':
-                    case 'application/vnd.elife.collection+json; version=2':
+                    case (string) new MediaType(CollectionsClient::TYPE_COLLECTION, Collections::VERSION_COLLECTION):
                         $data['content'] = $this->updateItems($data['content']);
                         $data['relatedContent'] = $this->updateItems($data['relatedContent'] ?? []);
                         break;
 
-                    case 'application/vnd.elife.cover-list+json; version=1':
-                    case 'application/vnd.elife.highlight-list+json; version=3':
+                    case (string) new MediaType(CoversClient::TYPE_COVERS_LIST, Covers::VERSION_COVERS_LIST):
+                    case (string) new MediaType(HighlightsClient::TYPE_HIGHLIGHT_LIST, Highlights::VERSION_HIGHLIGHT_LIST):
                         $data['items'] = array_map(function (array $cover) {
                             $cover['item'] = $this->updateItem($cover['item']);
 
@@ -75,7 +90,7 @@ final class StatusDateOverrideMiddleware
                         }, $data['items']);
                         break;
 
-                    case 'application/vnd.elife.podcast-episode+json; version=1':
+                    case (string) new MediaType(PodcastClient::TYPE_PODCAST_EPISODE, PodcastEpisodes::VERSION_PODCAST_EPISODE):
                         $data['chapters'] = array_map(function (array $chapter) {
                             $chapter['content'] = $this->updateItems($chapter['content'] ?? []);
 
@@ -83,11 +98,11 @@ final class StatusDateOverrideMiddleware
                         }, $data['chapters']);
                         break;
 
-                    case 'application/vnd.elife.press-package+json; version=3':
+                    case (string) new MediaType(PressPackagesClient::TYPE_PRESS_PACKAGE, PressPackages::VERSION_PRESS_PACKAGE):
                         $data['relatedContent'] = $this->updateItems($data['relatedContent'] ?? []);
                         break;
 
-                    case 'application/vnd.elife.search+json; version=1':
+                    case (string) new MediaType(SearchClient::TYPE_SEARCH, Search::VERSION_SEARCH):
                         $data['items'] = $this->updateItems($data['items']);
                         break;
                 }
