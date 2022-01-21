c8368f60866a4e3a5bbc78ac6c7e9333de52c624
nlisgo <nathan@aeopus.com>
2022-01-21 17:59:53 +0000

    Amend description

--- src/Etoc/LatestArticles.php
+++ src/Etoc/LatestArticles.php
@@ -5,7 +5,7 @@ namespace eLife\Journal\Etoc;
 final class LatestArticles extends Newsletter
 {
     const LABEL = 'latest_articles';
-    const DESCRIPTION = 'Latest Articles';
+    const DESCRIPTION = 'latest research articles';
     const GROUP = 'All_Content_53';
     const GROUP_ID = 53;
     const UNSUBSCRIBE_FIELD = 'custom_132';
