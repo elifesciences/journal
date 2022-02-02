71ccb857dcc6143d27e31bc3c6d647aa900e96ec
nlisgo <nathan@aeopus.com>
2022-02-02 21:26:21 +0000

    Update descriptions

--- src/Etoc/EarlyCareer.php
+++ src/Etoc/EarlyCareer.php
@@ -5,7 +5,7 @@ namespace eLife\Journal\Etoc;
 final class EarlyCareer extends Newsletter
 {
     const LABEL = 'early_career';
-    const DESCRIPTION = 'Early Career';
+    const DESCRIPTION = 'the early-career researcher newsletter and Community updates';
     const GROUP = 'early_careers_news_317';
     const GROUP_ID = 317;
 }
--- src/Etoc/Technology.php
+++ src/Etoc/Technology.php
@@ -5,7 +5,7 @@ namespace eLife\Journal\Etoc;
 final class Technology extends Newsletter
 {
     const LABEL = 'technology';
-    const DESCRIPTION = 'Technology';
+    const DESCRIPTION = 'Technology news';
     const GROUP = 'technology_news_435';
     const GROUP_ID = 435;
 }
