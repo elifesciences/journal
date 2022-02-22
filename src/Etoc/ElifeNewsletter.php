b06e8ceca3df42b3d179759658639a473ce66a78
nlisgo <nathan@aeopus.com>
2022-02-22 12:02:31 +0000

    Amend descriptions

--- src/Etoc/ElifeNewsletter.php
+++ src/Etoc/ElifeNewsletter.php
@@ -5,7 +5,7 @@ namespace eLife\Journal\Etoc;
 final class ElifeNewsletter extends Newsletter
 {
     const LABEL = 'elife_newsletter';
-    const DESCRIPTION = 'eLife newsletter';
+    const DESCRIPTION = 'the eLife newsletter';
     const GROUP = 'eLife_bi_monthly_news_1032';
     const GROUP_ID = 1032;
 }
--- src/Etoc/Technology.php
+++ src/Etoc/Technology.php
@@ -5,7 +5,7 @@ namespace eLife\Journal\Etoc;
 final class Technology extends Newsletter
 {
     const LABEL = 'technology';
-    const DESCRIPTION = 'Technology news';
+    const DESCRIPTION = 'eLife technology news';
     const GROUP = 'technology_news_435';
     const GROUP_ID = 435;
 }
