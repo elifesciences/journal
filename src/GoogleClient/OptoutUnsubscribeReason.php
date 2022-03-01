bd1ee85129152a9b89d45eb8fe6de650eebcf56c
nlisgo <nathan@aeopus.com>
2022-03-01 12:37:29 +0000

    Add logger to OptoutUnsubscribeReason

--- src/GoogleClient/OptoutUnsubscribeReason.php
+++ src/GoogleClient/OptoutUnsubscribeReason.php
@@ -5,6 +5,7 @@ namespace eLife\Journal\GoogleClient;
 use eLife\Journal\Etoc\Newsletter;
 use Google\Service\Sheets;
 use Google\Service\Sheets\AppendValuesResponse;
+use Psr\Log\LoggerInterface;
 use Webmozart\Assert\Assert;
 
 final class OptoutUnsubscribeReason implements OptoutUnsubscribeReasonInterface
@@ -13,17 +14,22 @@ final class OptoutUnsubscribeReason implements OptoutUnsubscribeReasonInterface
     private $sheetId;
     private $refreshToken;
 
-    public function __construct(Sheets $sheets, string $sheetId, string $refreshToken)
+    public function __construct(Sheets $sheets, string $sheetId, string $refreshToken, LoggerInterface $logger)
     {
         $this->sheets = $sheets;
         $this->sheetId = $sheetId;
         $this->refreshToken = $refreshToken;
+
+        $this->sheets->getClient()->setLogger($logger);
     }
 
     public function record(array $reasons, $reasonOther, bool $optOut, Newsletter $newsletter = null) : AppendValuesResponse
     {
         Assert::true($optOut || $newsletter instanceof Newsletter, 'Opt-out must be true or Newsletter provided.');
+
+        // Access token refresh.
         $this->sheets->getClient()->fetchAccessTokenWithRefreshToken($this->refreshToken);
+
         return $this->sheets->spreadsheets_values->append(
             $this->sheetId,
             'A1:H1',
