ac10cc95597561f5b3e2d466ecdfee7f8eeee472
nlisgo <nathan@aeopus.com>
2022-03-14 18:44:33 +0000

    Create ArticleReferenceConverter

---
+++ src/ViewModel/Converter/ArticleReferenceConverter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace eLife\Journal\ViewModel\Converter;
+
+use eLife\ApiSdk\Model\ArticleVersion;
+use eLife\ApiSdk\Model\AuthorEntry;
+use eLife\Journal\ViewModel\Converter\Reference\HasAuthors;
+use eLife\Patterns\ViewModel;
+
+final class ArticleReferenceConverter implements ViewModelConverter
+{
+    use HasAuthors;
+
+    /**
+     * @param ArticleVersion $object
+     */
+    public function convert($object, string $viewModel = null, array $context = []) : ViewModel
+    {
+        $authors = $object->getAuthors()->notEmpty() ? [new ViewModel\ReferenceAuthorList($object->getAuthors()->map(function (AuthorEntry $author) {
+            return ViewModel\Author::asText($author->toString());
+        })->toArray(), $object->getPublishedDate()->format('Y'))] : [];
+
+        return ViewModel\Reference::withDoi($object->getFullTitle(), new ViewModel\Doi($object->getDoi()), null, null, [], $authors, []);
+    }
+
+    public function supports($object, string $viewModel = null, array $context = []) : bool
+    {
+        return $object instanceof ArticleVersion && ViewModel\Reference::class === $viewModel;
+    }
+}
