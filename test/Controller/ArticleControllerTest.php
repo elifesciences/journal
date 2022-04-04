7d79d8fedde5022c4890033c16cb1d1fefe2671b
Alfred <alfred-ci@elifesciences.org>
2022-04-05 05:04:45 +0930

    Updated elife/patterns to 9535826: Updated pattern-library to 8a573f4: Allow for intro in download links (#1597)

--- test/Controller/ArticleControllerTest.php
+++ test/Controller/ArticleControllerTest.php
@@ -26,7 +26,7 @@ final class ArticleControllerTest extends PageTestCase
 
         $this->assertContains('Cite this article as: eLife 2010;1:e00001',
             preg_replace('!\s+!', ' ', $crawler->filter('.contextual-data__cite_wrapper')->text()));
-        $this->assertContains('doi: 10.7554/eLife.00001', $crawler->filter('.contextual-data__cite_wrapper')->text());
+        $this->assertContains('doi: 10.7554/eLife.00001', $this->crawlerText($crawler->filter('.contextual-data__cite_wrapper')));
     }
 
     /**
@@ -2469,13 +2469,13 @@ final class ArticleControllerTest extends PageTestCase
         $this->assertSame('Abstract text',
             $crawler->filter('.grid-column > section:nth-of-type(1) > div > p')->text());
         $this->assertSame('https://doi.org/10.7554/eLife.09560.001',
-            $crawler->filter('.grid-column > section:nth-of-type(1) > div > .doi')->text());
+            $this->crawlerText($crawler->filter('.grid-column > section:nth-of-type(1) > div > .doi')));
         $this->assertSame('Editor\'s evaluation',
             $crawler->filter('.grid-column > section:nth-of-type(2) > header > h2')->text());
         $this->assertSame('Editor\'s evaluation text',
             $crawler->filter('.grid-column > section:nth-of-type(2) > div > p')->text());
         $this->assertSame('https://doi.org/10.7554/eLife.09560.sa0',
-            $crawler->filter('.grid-column > section:nth-of-type(2) > div > .doi')->text());
+            $this->crawlerText($crawler->filter('.grid-column > section:nth-of-type(2) > div > .doi')));
         $this->assertSame(
             [
                 [
@@ -2498,7 +2498,7 @@ final class ArticleControllerTest extends PageTestCase
         $this->assertSame('Digest text',
             $crawler->filter('.grid-column > section:nth-of-type(3) > div > p')->text());
         $this->assertSame('https://doi.org/10.7554/eLife.09560.002',
-            $crawler->filter('.grid-column > section:nth-of-type(3) > div > .doi')->text());
+            $this->crawlerText($crawler->filter('.grid-column > section:nth-of-type(3) > div > .doi')));
         $this->assertSame('Body title',
             $crawler->filter('.grid-column > section:nth-of-type(4) > header > h2')->text());
 
