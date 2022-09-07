8bed904013776896a49e541905c47f1196829b34
SaMoghadamzadeh <saeed.moghadamzadeh1@endava.com>
2022-09-07 14:50:42 +0200

    fix acceptance tests

--- features/bootstrap/SubjectContext.php
+++ features/bootstrap/SubjectContext.php
@@ -236,7 +236,7 @@ final class SubjectContext extends Context
         $this->mockApiResponse(
             new Request(
                 'GET',
-                "http://api.elifesciences.org/search?for=&page=1&per-page=1&sort=date&order=desc&subject[]=$subjectId&type[]=research-article&type[]=research-communication&type[]=research-advance&type[]=review-article&type[]=scientific-correspondence&type[]=short-report&type[]=tools-resources&type[]=replication-study&type[]=editorial&type[]=insight&type[]=feature&type[]=collection&use-date=default",
+                "http://api.elifesciences.org/search?for=&page=1&per-page=1&sort=date&order=desc&subject[]=$subjectId&type[]=reviewed-preprinte&type[]=research-article&type[]=research-communication&type[]=research-advance&type[]=review-article&type[]=scientific-correspondence&type[]=short-report&type[]=tools-resources&type[]=replication-study&type[]=editorial&type[]=insight&type[]=feature&type[]=collection&use-date=default",
                 ['Accept' => 'application/vnd.elife.search+json; version=2']
             ),
             new Response(
@@ -290,7 +290,7 @@ final class SubjectContext extends Context
             $this->mockApiResponse(
                 new Request(
                     'GET',
-                    "http://api.elifesciences.org/search?for=&page=$page&per-page=$chunk&sort=date&order=desc&subject[]=$subjectId&type[]=research-article&type[]=research-communication&type[]=research-advance&type[]=review-article&type[]=scientific-correspondence&type[]=short-report&type[]=tools-resources&type[]=replication-study&type[]=editorial&type[]=insight&type[]=feature&type[]=collection&use-date=default",
+                    "http://api.elifesciences.org/search?for=&page=$page&per-page=$chunk&sort=date&order=desc&subject[]=$subjectId&type[]=reviewed-preprint&type[]=research-article&type[]=research-communication&type[]=research-advance&type[]=review-article&type[]=scientific-correspondence&type[]=short-report&type[]=tools-resources&type[]=replication-study&type[]=editorial&type[]=insight&type[]=feature&type[]=collection&use-date=default",
                     ['Accept' => 'application/vnd.elife.search+json; version=2']
                 ),
                 new Response(
@@ -332,7 +332,7 @@ final class SubjectContext extends Context
                             'interview' => 0,
                             'labs-post' => 0,
                             'podcast-episode' => 0,
-                             'reviewed-preprint' => 0,
+                            'reviewed-preprint' => 0,
                         ],
                     ])
                 )
@@ -496,7 +496,7 @@ final class SubjectContext extends Context
             $this->mockApiResponse(
                 new Request(
                     'GET',
-                    "http://api.elifesciences.org/search?for=&page=1&per-page=10&sort=date&order=desc&subject[]=$subjectId&type[]=research-article&type[]=research-communication&type[]=research-advance&type[]=review-article&type[]=scientific-correspondence&type[]=short-report&type[]=tools-resources&type[]=replication-study&type[]=editorial&type[]=insight&type[]=feature&type[]=collection&use-date=default",
+                    "http://api.elifesciences.org/search?for=&page=1&per-page=10&sort=date&order=desc&subject[]=$subjectId&type[]=reviewed-preprint&type[]=research-article&type[]=research-communication&type[]=research-advance&type[]=review-article&type[]=scientific-correspondence&type[]=short-report&type[]=tools-resources&type[]=replication-study&type[]=editorial&type[]=insight&type[]=feature&type[]=collection&use-date=default",
                     ['Accept' => 'application/vnd.elife.search+json; version=2']
                 ),
                 new Response(
