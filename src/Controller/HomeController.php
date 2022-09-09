567dfe20097cb9c84327e093d4b309f272e0622c
nlisgo <nathan@aeopus.com>
2022-09-09 18:51:32 +0100

    Limit cover to 3 for carousel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -74,7 +74,7 @@ final class HomeController extends Controller
             ->getCurrent()
             ->map($this->willConvertTo(CarouselItem::class))
             ->then(Callback::emptyOr(function (Sequence $covers) {
-                return new Carousel($covers->toArray(), new ListHeading('Highlights', 'highlights'));
+                return new Carousel($covers->slice(0, 3)->toArray(), new ListHeading('Highlights', 'highlights'));
             }))
             ->otherwise($this->softFailure('Failed to load covers'));
 
