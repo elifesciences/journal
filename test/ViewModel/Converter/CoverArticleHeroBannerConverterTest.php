338c9baa214b2f4598b82bc9c908795cf4fc1162
SaMoghadamzadeh <saeed.moghadamzadeh1@endava.com>
2022-09-27 14:47:15 +0200

    Add hero banner converters unittests

---
+++ test/ViewModel/Converter/CoverArticleHeroBannerConverterTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace test\eLife\Journal\ViewModel\Converter;
+
+use eLife\ApiSdk\Model\ArticleVersion;
+use eLife\ApiSdk\Model\Cover;
+use eLife\ApiSdk\Model\Model;
+use eLife\Journal\ViewModel\Converter\CoverArticleHeroBannerConverter;
+use eLife\Patterns\ViewModel\HeroBanner;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Traversable;
+
+final class CoverArticleHeroBannerConverterTest extends ModelConverterTestCase
+{
+    protected $models = ['cover'];
+    protected $viewModelClasses = [HeroBanner::class];
+
+    /**
+     * @before
+     */
+    public function setUpConverter()
+    {
+        $urlGenerator = $this->createMock(UrlGeneratorInterface::class);
+        $urlGenerator
+            ->expects($this->any())
+            ->method('generate')
+            ->will($this->returnValue('/'));
+
+        $this->converter = new CoverArticleHeroBannerConverter(
+            $urlGenerator
+        );
+    }
+
+    /**
+     * @param Cover $model
+     */
+    protected function modelHook(Model $model) : Traversable
+    {
+        if ($model->getItem() instanceof ArticleVersion) {
+            yield $model;
+        }
+    }
+}
