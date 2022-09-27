1b829e049219e072c45b5c6d8e7a40b9cf222de0
SaMoghadamzadeh <saeed.moghadamzadeh1@endava.com>
2022-09-27 14:44:58 +0200

    Add hero banner converters

---
+++ src/ViewModel/Converter/CoverCollectionHeroBannerConverter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace eLife\Journal\ViewModel\Converter;
+
+use eLife\ApiSdk\Model\Collection;
+use eLife\ApiSdk\Model\Cover;
+use eLife\ApiSdk\Model\Subject;
+use eLife\Journal\ViewModel\Factory\CarouselItemImageFactory;
+use eLife\Journal\ViewModel\Factory\PictureBuilderFactory;
+use eLife\Patterns\ViewModel;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+final class CoverCollectionHeroBannerConverter implements ViewModelConverter
+{
+    use CreatesDate;
+
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    /**
+     * @param Cover $object
+     */
+    public function convert($object, string $viewModel = null, array $context = []) : ViewModel
+    {
+        /** @var Collection $collection */
+        $collection = $object->getItem();
+
+        return new ViewModel\HeroBanner(
+            $collection->getSubjects()->map(function (Subject $subject) {
+                return new ViewModel\Link($subject->getName(), $this->urlGenerator->generate('subject', [$subject]));
+            })->toArray(),
+            new ViewModel\Link($object->getTitle(), $this->urlGenerator->generate('collection', [$collection])),
+            ViewModel\Meta::withLink(new ViewModel\Link('Collection', $this->urlGenerator->generate('collections')), $this->simpleDate($collection, $context)),
+            (new PictureBuilderFactory())->forImage(
+                $object->getBanner(), $object->getBanner()->getWidth()
+            )->build(),
+            $collection->getImpactStatement()
+        );
+    }
+
+    public function supports($object, string $viewModel = null, array $context = []) : bool
+    {
+        return $object instanceof Cover && ViewModel\HeroBanner::class === $viewModel && $object->getItem() instanceof Collection;
+    }
+}
