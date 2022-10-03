5504c46fc4deca870e71a41013982b908162df29
David Moulton <d.moulton@elifesciences.org>
2022-10-03 15:09:24 +0100

    Add feature flagged peer review process page

--- src/Controller/PeerReviewProcessController.php
+++ src/Controller/PeerReviewProcessController.php
@@ -6,10 +6,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-final class PeerReviewProcessController {
+final class PeerReviewProcessController extends Controller {
 
-  public function peerReviewProcessAction(Request $request) : Response
-  {
+  public function peerReviewProcessAction(Request $request): Response {
+    if (!$this->isGranted('FEATURE_PRC_COMMS')) {
+      throw new NotFoundHttpException('Not allowed to see PRC comms');
+    }
+
     throw new NotFoundHttpException('Nope');
   }
 }
