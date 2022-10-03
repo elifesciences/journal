fc071a8f0e9f2f99a7a38ed0d937217ae5fb66e9
David Moulton <d.moulton@elifesciences.org>
2022-10-03 15:41:43 +0100

    Add first pass content header to peer review process page

--- src/Controller/PeerReviewProcessController.php
+++ src/Controller/PeerReviewProcessController.php
@@ -5,6 +5,8 @@ namespace eLife\Journal\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use eLife\Patterns\ViewModel\ContentHeader;
+
 
 final class PeerReviewProcessController extends Controller {
 
@@ -15,7 +17,9 @@ final class PeerReviewProcessController extends Controller {
 
     $arguments = $this->defaultPageArguments($request);
 
-    $arguments['title'] = 'Peer review process';
+    $arguments['title'] = 'eLife’s peer review process';
+
+    $arguments['contentHeader'] = new ContentHeader($arguments['title'], null, 'eLife publishes the outcome of every peer-review so that the evaluation of research is transparent and fair. Editors and Reviewers consult to agree on the outcomes but do not evaluate research based on suitability for publication.<p>To learn more about why eLife’s process is changing, read the Editorial.</p>');
 
     return new Response($this->get('templating')->render('::peer-review-process.html.twig', $arguments));
   }
