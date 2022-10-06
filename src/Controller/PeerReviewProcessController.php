47c65d128ec39667c353ada9d67a3e7b63ceaaf9
nlisgo <nathan@aeopus.com>
2022-10-06 18:54:52 +0100

    cs

--- src/Controller/PeerReviewProcessController.php
+++ src/Controller/PeerReviewProcessController.php
@@ -7,20 +7,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use eLife\Patterns\ViewModel\ContentHeader;
 
+final class PeerReviewProcessController extends Controller
+{
+    public function peerReviewProcessAction(Request $request): Response
+    {
+        if (!$this->isGranted('FEATURE_PRC_COMMS')) {
+            throw new NotFoundHttpException('Not allowed to see PRC comms');
+        }
 
-final class PeerReviewProcessController extends Controller {
+        $arguments = $this->defaultPageArguments($request);
 
-  public function peerReviewProcessAction(Request $request): Response {
-    if (!$this->isGranted('FEATURE_PRC_COMMS')) {
-      throw new NotFoundHttpException('Not allowed to see PRC comms');
+        $arguments['title'] = 'eLife’s peer review process';
+
+        $arguments['contentHeader'] = new ContentHeader($arguments['title'], null, 'eLife publishes the outcome of every peer-review so that the evaluation of research is transparent and fair. Editors and Reviewers consult to agree on the outcomes but do not evaluate research based on suitability for publication.<p>To learn more about why eLife’s process is changing, <a href="#" class="peer-review-process-content-header-link">read the Editorial</a>.</p>');
+
+        return new Response($this->get('templating')->render('::peer-review-process.html.twig', $arguments));
     }
-
-    $arguments = $this->defaultPageArguments($request);
-
-    $arguments['title'] = 'eLife’s peer review process';
-
-    $arguments['contentHeader'] = new ContentHeader($arguments['title'], null, 'eLife publishes the outcome of every peer-review so that the evaluation of research is transparent and fair. Editors and Reviewers consult to agree on the outcomes but do not evaluate research based on suitability for publication.<p>To learn more about why eLife’s process is changing, <a href="#" class="peer-review-process-content-header-link">read the Editorial</a>.</p>');
-
-    return new Response($this->get('templating')->render('::peer-review-process.html.twig', $arguments));
-  }
 }
