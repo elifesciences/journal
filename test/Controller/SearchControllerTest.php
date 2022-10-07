4ba921dabd39ba97534e276d8241dba3dc9eed70
SaMoghadamzadeh <saeed.moghadamzadeh1@endava.com>
2022-10-07 14:43:09 +0200

    Add test for SearchController.

--- test/Controller/SearchControllerTest.php
+++ test/Controller/SearchControllerTest.php
@@ -338,7 +338,7 @@ final class SearchControllerTest extends PageTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', $this->getUrl().'?foo');
+        $crawler = $client->request('GET', $this->getUrl() . '?foo');
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
 
@@ -349,8 +349,8 @@ final class SearchControllerTest extends PageTestCase
         $this->assertEmpty($crawler->filter('meta[property="og:description"]'));
         $this->assertEmpty($crawler->filter('meta[name="description"]'));
         $this->assertSame('summary', $crawler->filter('meta[name="twitter:card"]')->attr('content'));
-        $this->assertSame('http://localhost/'.ltrim(self::$kernel->getContainer()->get('elife.assets.packages')->getUrl('assets/images/social/icon-600x600@1.png'), '/'), $crawler->filter('meta[name="twitter:image"]')->attr('content'));
-        $this->assertSame('http://localhost/'.ltrim(self::$kernel->getContainer()->get('elife.assets.packages')->getUrl('assets/images/social/icon-600x600@1.png'), '/'), $crawler->filter('meta[property="og:image"]')->attr('content'));
+        $this->assertSame('http://localhost/' . ltrim(self::$kernel->getContainer()->get('elife.assets.packages')->getUrl('assets/images/social/icon-600x600@1.png'), '/'), $crawler->filter('meta[name="twitter:image"]')->attr('content'));
+        $this->assertSame('http://localhost/' . ltrim(self::$kernel->getContainer()->get('elife.assets.packages')->getUrl('assets/images/social/icon-600x600@1.png'), '/'), $crawler->filter('meta[property="og:image"]')->attr('content'));
         $this->assertSame('600', $crawler->filter('meta[property="og:image:width"]')->attr('content'));
         $this->assertSame('600', $crawler->filter('meta[property="og:image:height"]')->attr('content'));
         $this->assertEmpty($crawler->filter('meta[name="dc.identifier"]'));
@@ -361,7 +361,162 @@ final class SearchControllerTest extends PageTestCase
         $this->assertEmpty($crawler->filter('meta[name="dc.rights"]'));
     }
 
-    protected function getUrl() : string
+    /**
+     * @test
+     */
+    public function it_shows_reviewed_preprints_on_results()
+    {
+
+        $client = static::createClient();
+
+        $items = [
+            [
+                "id" => "19560",
+                "type" => "reviewed-preprint",
+                "doi" => "10.7554/eLife.19560",
+                "status" => "reviewed",
+                "authorLine" => "Lee R Berger et al.",
+                "title" => "reviewed preprint title",
+                "indexContent" => 'reviewed preprint index content',
+                "titlePrefix" => "Title prefix",
+                "stage" => "published",
+                "published" => "2022-08-01T00:00:00Z",
+                "reviewedDate" => "2022-08-01T00:00:00Z",
+                "statusDate" => "2022-08-01T00:00:00Z",
+                "volume" => 4,
+                "elocationId" => "e19560",
+                "pdf" => "https://elifesciences.org/content/4/e19560.pdf",
+                "subjects" => [
+                    [
+                        "id" => "genomics-evolutionary-biology",
+                        "name" => "Genomics and Evolutionary Biology",
+                    ]
+                ],
+                "curationLabels" => [
+                    "Ground-breaking",
+                    "Convincing"
+                ],
+                "image" => [
+                    "thumbnail" => [
+                        "uri" => "https://iiif.elifesciences.org/lax/19560%2Felife-19560-fig1-v1.tif",
+                        "alt" => "",
+                        "source" => [
+                            "mediaType" => "image/jpeg",
+                            "uri" => "https://iiif.elifesciences.org/lax/19560%2Felife-19560-fig1-v1.tif/full/full/0/default.jpg",
+                            "filename" => "an-image.jpg"
+                        ],
+                        "size" => [
+                            "width" => 4194,
+                            "height" => 4714
+                        ],
+                        "focalPoint" => [
+                            "x" => 25,
+                            "y" => 75
+                        ]
+                    ]
+                ],
+            ],
+        ];
+
+        $this->mockApiResponse(
+            new Request(
+                'GET',
+                'http://api.elifesciences.org/search?for=Reviewed preprint&page=1&per-page=1&sort=relevance&order=desc&use-date=default',
+                ['Accept' => 'application/vnd.elife.search+json; version=2']
+            ),
+            new Response(
+                200,
+                ['Content-Type' => 'application/vnd.elife.search+json; version=2'],
+                json_encode([
+                    'total' => 1,
+                    'items' => $items,
+                    'subjects' => [
+                        [
+                            'id' => 'subject',
+                            'name' => 'Some subject',
+                            'results' => 0,
+                        ],
+                    ],
+                    'types' => [
+                        'correction' => 0,
+                        'editorial' => 0,
+                        'feature' => 0,
+                        'insight' => 0,
+                        'research-advance' => 0,
+                        'research-article' => 0,
+                        'research-communication' => 0,
+                        'retraction' => 0,
+                        'registered-report' => 0,
+                        'replication-study' => 0,
+                        'review-article' => 0,
+                        'scientific-correspondence' => 0,
+                        'short-report' => 0,
+                        'tools-resources' => 0,
+                        'blog-article' => 0,
+                        'collection' => 0,
+                        'interview' => 0,
+                        'labs-post' => 0,
+                        'podcast-episode' => 0,
+                        'reviewed-preprint' => 1,
+                    ],
+                ])
+            )
+        );
+        $this->mockApiResponse(
+            new Request(
+                'GET',
+                'http://api.elifesciences.org/search?for=Reviewed preprint&page=1&per-page=10&sort=relevance&order=desc&use-date=default',
+                ['Accept' => 'application/vnd.elife.search+json; version=2']
+            ),
+            new Response(
+                200,
+                ['Content-Type' => 'application/vnd.elife.search+json; version=2'],
+                json_encode([
+                    'total' => 1,
+                    'items' => $items,
+                    'subjects' => [
+                        [
+                            'id' => 'subject',
+                            'name' => 'Some subject',
+                            'results' => 0,
+                        ],
+                    ],
+                    'types' => [
+                        'correction' => 0,
+                        'editorial' => 0,
+                        'feature' => 0,
+                        'insight' => 0,
+                        'research-advance' => 0,
+                        'research-article' => 0,
+                        'research-communication' => 0,
+                        'retraction' => 0,
+                        'registered-report' => 0,
+                        'replication-study' => 0,
+                        'review-article' => 0,
+                        'scientific-correspondence' => 0,
+                        'short-report' => 0,
+                        'tools-resources' => 0,
+                        'blog-article' => 0,
+                        'collection' => 0,
+                        'interview' => 0,
+                        'labs-post' => 0,
+                        'podcast-episode' => 0,
+                        'reviewed-preprint' => 1,
+                    ],
+                ])
+            )
+        );
+
+        $crawler = $client->request('GET', '/search?for=Reviewed preprint&page=1&per-page=10&sort=relevance&order=desc&use-date=default');
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $listing = $crawler->filter('ol.listing-list > li');
+
+        $this->assertCount(1, $listing);
+        $this->assertSame('reviewed preprint title', trim($listing->eq(0)->filter('.teaser__header_text')->text()));
+        $this->assertSame('Reviewed Preprint Aug 1, 2022', trim(preg_replace('/\s+/S', ' ', $listing->eq(0)->filter('.teaser__footer .meta')->text())));
+    }
+
+    protected function getUrl(): string
     {
         $this->mockApiResponse(
             new Request(
