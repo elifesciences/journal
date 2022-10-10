500020a210b50ef7a8fef8e3f6f23c7796cc1aaf
nlisgo <nathan@aeopus.com>
2022-10-10 13:41:14 +0100

    Use new content header for inside elife

--- src/ViewModel/Converter/BlogArticleContentHeaderConverter.php
+++ src/ViewModel/Converter/BlogArticleContentHeaderConverter.php
@@ -5,7 +5,6 @@ namespace eLife\Journal\ViewModel\Converter;
 use eLife\ApiSdk\Model\BlogArticle;
 use eLife\Journal\Helper\LicenceUri;
 use eLife\Patterns\ViewModel;
-use eLife\Patterns\ViewModel\Link;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use function strip_tags;
 
@@ -25,23 +24,37 @@ final class BlogArticleContentHeaderConverter implements ViewModelConverter
      */
     public function convert($object, string $viewModel = null, array $context = []) : ViewModel
     {
-        return new ViewModel\ContentHeader(
-            $object->getTitle(), null, $object->getImpactStatement(), true, [],
-            null, null, null,
-            new ViewModel\SocialMediaSharers(
+        if ($date = $this->simpleDate($object, ['date' => 'published'] + $context)) {
+            $meta = ViewModel\MetaNew::withDate($date);
+        } else {
+            $meta = null;
+        }
+
+        return new ViewModel\ContentHeaderNew(
+            $object->getTitle(),
+            true, null, $object->getImpactStatement(), true,
+            new ViewModel\Breadcrumb([
+                new ViewModel\Link(
+                    'Inside eLife',
+                    $this->urlGenerator->generate('inside-elife')
+                ),
+            ]),
+            [], null, null, null, null,
+            new ViewModel\SocialMediaSharersNew(
                 strip_tags($object->getTitle()),
-                $this->urlGenerator->generate('inside-elife-article', [$object], UrlGeneratorInterface::ABSOLUTE_URL)
+                $this->urlGenerator->generate('inside-elife-article', [$object], UrlGeneratorInterface::ABSOLUTE_URL),
+                false
             ),
+            !empty($context['metrics']) ? ViewModel\ContextualData::withMetrics($context['metrics']) : null,
             null,
-            ViewModel\Meta::withLink(
-                new Link('Inside eLife', $this->urlGenerator->generate('inside-elife')),
-                $this->simpleDate($object, ['date' => 'published'] + $context)
-            ), LicenceUri::default()
+            $meta,
+            null,
+            LicenceUri::default()
         );
     }
 
     public function supports($object, string $viewModel = null, array $context = []) : bool
     {
-        return $object instanceof BlogArticle && ViewModel\ContentHeader::class === $viewModel;
+        return $object instanceof BlogArticle && ViewModel\ContentHeaderNew::class === $viewModel;
     }
 }
