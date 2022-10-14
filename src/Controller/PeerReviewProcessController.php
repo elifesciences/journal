6f72c081bd06ded8939a13967b27cc098efb5eb2
Nathan Lisgo <n.lisgo@elifesciences.org>
2022-10-14 15:36:35 +0100

    Enable peer review process page (#1686)

--- src/Controller/PeerReviewProcessController.php
+++ src/Controller/PeerReviewProcessController.php
@@ -11,15 +11,11 @@ final class PeerReviewProcessController extends Controller
 {
     public function peerReviewProcessAction(Request $request): Response
     {
-        if (!$this->isGranted('FEATURE_PRC_COMMS')) {
-            throw new NotFoundHttpException('Not allowed to see PRC comms');
-        }
-
         $arguments = $this->defaultPageArguments($request);
 
         $arguments['title'] = 'eLife’s peer review process';
 
-        $arguments['contentHeader'] = new ContentHeader($arguments['title'], null, 'eLife is changing its editorial process to emphasize public reviews and assessments of preprints by eliminating accept/reject decisions after peer review.<p>To learn more about why eLife’s process is changing, <a href="#" class="peer-review-process-content-header-link">read the Editorial</a>.</p>');
+        $arguments['contentHeader'] = new ContentHeader($arguments['title'], null, '<p>eLife is changing its editorial process to emphasize public reviews and assessments of preprints by eliminating accept/reject decisions after peer review.</p><p>To learn more about why eLife’s process is changing, <a href="#" class="peer-review-process-content-header-link">read the Editorial</a>.</p>');
 
         return new Response($this->get('templating')->render('::peer-review-process.html.twig', $arguments));
     }
