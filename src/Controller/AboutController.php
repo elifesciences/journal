fb6f512b637d1f39c00b236da0141c31787d02e1
Cosmin-Daniel Urzicianu <cosmindaniel.urzicianu@endava.com>
2022-10-24 12:08:18 +0300

    add new infoBar to all the about pages

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -47,8 +47,6 @@ final class AboutController extends Controller
         $arguments['contentHeader'] = new ContentHeader('eLife: evolution of publishing', null,
             'Independent, not-for-profit and supported by funders, eLife improves the way research is practised and shared.');
 
-        $arguments['infoBar'] = new InfoBar('eLife\'s peer-review process is changing. From January 2023, eLife will no longer make accept/reject decisions after peer review. Instead, every preprint sent for peer review will be published on the eLife website as a “Reviewed Preprint” that includes an eLife assessment, public reviews, and a response from the authors (if available). When writing the eLife assessment, the editors and reviewers will use a common vocabulary to summarize the significance of the findings and the strength of the evidence reported in the preprint. <a href="'.$this->get('router')->generate('inside-elife-article', ['id' => '54d63486']).'">Read about the new process</a>.', InfoBar::TYPE_ANNOUNCEMENT);
-
         $arguments['body'] = [
             new Paragraph('eLife is committed to creating a future where a diverse, global community of researchers shares open results for the benefit of all.'),
             new Paragraph('From the research we publish, to the tools we build, to the people we work with, we’ve earned a reputation for quality, integrity and the flexibility to bring about real change.'),
@@ -406,6 +404,7 @@ final class AboutController extends Controller
     private function aboutPageArguments(Request $request) : array
     {
         $arguments = $this->defaultPageArguments($request);
+        $arguments['infoBar'] = new InfoBar('eLife\'s peer-review process is changing. From January 2023, eLife will no longer make accept/reject decisions after peer review. Instead, every preprint sent for peer review will be published on the eLife website as a “Reviewed Preprint” that includes an eLife assessment, public reviews, and a response from the authors (if available). When writing the eLife assessment, the editors and reviewers will use a common vocabulary to summarize the significance of the findings and the strength of the evidence reported in the preprint. <a href="'.$this->get('router')->generate('inside-elife-article', ['id' => '54d63486']).'">Read about the new process</a>.', InfoBar::TYPE_ANNOUNCEMENT);
 
         $arguments['menuLink'] = new SectionListingLink('All sections', 'sections');
 
