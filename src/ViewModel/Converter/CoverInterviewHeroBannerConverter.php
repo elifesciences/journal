907bd826eb2605c36df5905a459917fa2a558067
Saeed Moghadamzadeh <saeed.moghadamzadeh@endava.com>
2022-11-07 11:48:59 +0100

    Add support for interviews on hero banner

---
+++ src/ViewModel/Converter/CoverInterviewHeroBannerConverter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace eLife\Journal\ViewModel\Converter;
+
+use eLife\ApiSdk\Model\Cover;
+use eLife\ApiSdk\Model\Interview;
+use eLife\Journal\Helper\ModelName;
+use eLife\Journal\ViewModel\Factory\PictureBuilderFactory;
+use eLife\Patterns\ViewModel;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+final class CoverInterviewHeroBannerConverter implements ViewModelConverter
+{
+    use CreatesDate;
+
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    /**
+     * @param Cover $object
+     */
+    public function convert($object, string $viewModel = null, array $context = []) : ViewModel
+    {
+        /** @var Interview $interview */
+        $interview = $object->getItem();
+
+        return new ViewModel\HeroBanner(
+            null,
+            new ViewModel\Link($object->getTitle(), $this->urlGenerator->generate('interview', [$interview])),
+            ViewModel\Meta::withLink(
+                new ViewModel\Link(
+                    ModelName::singular('interview'),
+                    $this->urlGenerator->generate('interviews')
+                ),
+                $this->simpleDate($interview, $context)
+            ),
+            (new PictureBuilderFactory())->forImage(
+                $object->getBanner(), 633, 367
+            )->build(),
+            $object->getImpactStatement(),
+            null
+        );
+    }
+
+    public function supports($object, string $viewModel = null, array $context = []) : bool
+    {
+        return $object instanceof Cover && ViewModel\HeroBanner::class === $viewModel && $object->getItem() instanceof Interview;
+    }
+}
