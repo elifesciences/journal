60632263df231730190babc8fe1759013b90f869
Cosmin-Daniel Urzicianu <cosmindaniel.urzicianu@endava.com>
2023-01-26 17:50:31 +0200

    fix related items context label and add further reading

--- src/ViewModel/Converter/ContentAsideConverter.php
+++ src/ViewModel/Converter/ContentAsideConverter.php
@@ -2,19 +2,12 @@
 
 namespace eLife\Journal\ViewModel\Converter;
 
-use eLife\ApiSdk\Collection\Sequence;
-use eLife\ApiSdk\Model\Article;
 use eLife\ApiSdk\Model\ArticleVersion;
 use eLife\Journal\Helper\CanConvertContent;
 use eLife\Journal\Helper\DownloadLinkUriGenerator;
 use eLife\Patterns\ViewModel;
 use eLife\Patterns\ViewModel\Button;
-use eLife\Patterns\ViewModel\ContextualData;
-use eLife\Patterns\ViewModel\Link;
-use eLife\Patterns\ViewModel\ListingTeasers;
-use eLife\Patterns\ViewModel\Teaser;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use function GuzzleHttp\Promise\all;
 
 final class ContentAsideConverter implements ViewModelConverter
 {
@@ -37,19 +30,6 @@ final class ContentAsideConverter implements ViewModelConverter
      */
     public function convert($object, string $viewModel = null, array $context = []) : ViewModel
     {
-        if (!empty($context['metrics'])) {
-            $contextualData = ContextualData::withMetrics($context['metrics']);
-        }
-
-        $relatedItem = null;
-        if (!empty($context['relatedItem'])) {
-            $item = $this->convertTo($context['relatedItem'], Teaser::class, [
-                'variant' => 'relatedItem',
-                'from' => $object->getType(),
-            ]);
-            $relatedItem = ListingTeasers::basic([$item]);
-        }
-
 
         return new ViewModel\ContentAside(
             new ViewModel\ContentAsideStatus('Version of Record',
@@ -61,9 +41,9 @@ final class ContentAsideConverter implements ViewModelConverter
                 Button::action('Share', '#share', true, 'button-action-share', Button::ACTION_VARIANT_SHARE),
                 Button::action('Comment<span aria-hidden=\'true\'><span data-visible-annotation-count></span> </span><span class=\'visuallyhidden\'>Open annotations (there are currently <span data-hypothesis-annotation-count>0</span> annotations on this page). </span>', '#comment', true, null, Button::ACTION_VARIANT_COMMENT),
             ], true),
-            $contextualData ?? null,
+            !empty($context['metrics']) ? ViewModel\ContextualData::withMetrics($context['metrics']) : null,
             !empty($context['timeline']) ? ViewModel\DefinitionList::timeline($context['timeline']) : null,
-            $relatedItem
+            $context['relatedItem'] ?? null
         );
     }
 
