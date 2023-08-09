e198731dfe7c1ee7f7fd1f3c97d400fa0d1b65bf
Viktoria Nedyalkova <viktoria.nedyalkova@endava.com>
2023-08-09 11:16:39 +0300

    Fix tests

--- test/ViewModel/Converter/InterviewContentHeaderConverterTest.php
+++ test/ViewModel/Converter/InterviewContentHeaderConverterTest.php
@@ -4,12 +4,12 @@ namespace test\eLife\Journal\ViewModel\Converter;
 
 use eLife\ApiSdk\Model\Interview;
 use eLife\Journal\ViewModel\Converter\InterviewContentHeaderConverter;
-use eLife\Patterns\ViewModel\ContentHeader;
+use eLife\Patterns\ViewModel\ContentHeaderNew;
 
 final class InterviewContentHeaderConverterTest extends ModelConverterTestCase
 {
     protected $models = ['interview'];
-    protected $viewModelClasses = [ContentHeader::class];
+    protected $viewModelClasses = [ContentHeaderNew::class];
 
     /**
      * @before
