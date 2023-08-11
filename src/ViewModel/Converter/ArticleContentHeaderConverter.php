3906b7c6377e62e00bb61fbde28cbeda7fce61cc
Saeed <smoqadam@users.noreply.github.com>
2023-08-11 10:43:39 +0200

    Display DOI version in content header (#1819)

--- src/ViewModel/Converter/ArticleContentHeaderConverter.php
+++ src/ViewModel/Converter/ArticleContentHeaderConverter.php
@@ -6,6 +6,7 @@ use eLife\ApiSdk\Model\ArticleVersion;
 use eLife\ApiSdk\Model\HasImpactStatement;
 use eLife\ApiSdk\Model\Subject;
 use eLife\Journal\Helper\CanConvertContent;
+use eLife\Journal\Helper\DoiVersion;
 use eLife\Journal\Helper\LicenceUri;
 use eLife\Journal\Helper\ModelName;
 use eLife\Patterns\ViewModel;
@@ -61,6 +62,8 @@ final class ArticleContentHeaderConverter implements ViewModelConverter
             $meta = null;
         }
 
+        $doi = (string) new DoiVersion($object);
+
         return new ViewModel\ContentHeaderNew(
             $object->getFullTitle(),
             !$isMagazineOrFeature,
@@ -76,14 +79,14 @@ final class ArticleContentHeaderConverter implements ViewModelConverter
             '#cite-this-article',
             new ViewModel\SocialMediaSharersNew(
                 strip_tags($object->getFullTitle()),
-                "https://doi.org/{$object->getDoi()}",
+                "https://doi.org/{$doi}",
                 true,
                 true
             ),
             !empty($context['metrics']) ? ViewModel\ContextualData::withMetrics($context['metrics']) : null,
             null,
             $meta,
-            $object->getDoi() ? new ViewModel\Doi($object->getDoi()) : null,
+            new ViewModel\Doi($doi),
             LicenceUri::forCode($object->getCopyright()->getLicense())
         );
     }
