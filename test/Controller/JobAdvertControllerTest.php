97345549e629cf7e698d374bcc5a4304ce3f58c4
Viktoria Nedyalkova <viktoria.nedyalkova@endava.com>
2023-08-22 14:08:29 +0300

    Remove metrics test

--- test/Controller/JobAdvertControllerTest.php
+++ test/Controller/JobAdvertControllerTest.php
@@ -34,57 +34,6 @@ final class JobAdvertControllerTest extends PageTestCase
     /**
      * @test
      */
-    public function it_displays_metrics()
-    {
-        $client = static::createClient();
-
-        $this->mockApiResponse(
-            new Request(
-                'GET',
-                'http://api.elifesciences.org/metrics/job-advert/1/page-views?by=month&page=1&per-page=20&order=desc',
-                ['Accept' => 'application/vnd.elife.metric-time-period+json; version=1']
-            ),
-            new Response(
-                200,
-                ['Content-Type' => 'application/vnd.elife.metric-time-period+json; version=1'],
-                json_encode([
-                    'totalPeriods' => 2,
-                    'totalValue' => 5678,
-                    'periods' => [
-                        [
-                            'period' => '2016-01-01',
-                            'value' => 2839,
-                        ],
-                        [
-                            'period' => '2016-01-02',
-                            'value' => 2839,
-                        ],
-                    ],
-                ])
-            )
-        );
-
-        $crawler = $client->request('GET', $this->getUrl());
-
-        $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $this->assertSame('Job advert title', $crawler->filter('.content-header__title')->text());
-
-        $this->assertSame('Open annotations (there are currently 0 annotations on this page).',
-        $this->crawlerText($crawler->filter('.wrapper--content .side-section-wrapper__link')));
-
-        $this->assertSame(
-            [
-                '5,678 views',
-            ],
-            array_map(function (string $text) {
-                return trim(preg_replace('!\s+!', ' ', $text));
-            }, $crawler->filter('.contextual-data__item')->extract('_text'))
-        );
-    }
-
-    /**
-     * @test
-     */
     public function it_has_metadata()
     {
         $client = static::createClient();
