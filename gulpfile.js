e4c69c610bd358cc32e3a1ac28cdb32daa3b964c
Scott Aubrey <scott@aubrey.org.uk>
2024-10-11 15:02:44 +0100

    revert workaround for image building in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -101,10 +101,6 @@ gulp.task('images:banners', () => {
         .pipe(gulp.dest('./build/assets/images/banners'));
 });
 
-gulp.task('images:banners:skipped', () => {
-    return Promise.resolve('');
-});
-
 gulp.task('images:social', () => {
     return gulp.src('./assets/images/social/*.png')
         .pipe(responsive({
@@ -207,9 +203,7 @@ gulp.task('images:svgs', () => {
         .pipe(gulp.dest('./build/assets/images'));
 });
 
-const imageBannersTaskName = process.env.SKIP_IMAGE_BANNERS === 'yes' ? 'images:banners:skipped' : 'images:banners';
-
-gulp.task('images', gulp.series('images:clean', imageBannersTaskName, 'images:social', 'images:investors', 'images:svgs', 'images:logos', () => {
+gulp.task('images', gulp.series('images:clean', 'images:banners', 'images:social', 'images:investors', 'images:svgs', 'images:logos', () => {
     return gulp.src('./build/assets/images/**/*')
         .pipe(imageMin([
             imageMinMozjpeg({
