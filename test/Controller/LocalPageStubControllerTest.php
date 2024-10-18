ea655bc4fab40feebf0946ba9db1f55b2b42aba6
Giorgio Sironi <g.sironi@elifesciences.org>
2024-10-18 10:30:39 +0100

    Begin to make it possible to test stubbed page behavior with different paths elifesciences/issues#8964

--- test/Controller/LocalPageStubControllerTest.php
+++ test/Controller/LocalPageStubControllerTest.php
@@ -9,15 +9,23 @@ final class LocalPageStubControllerTest extends PageTestCase
 {
     /**
      * @test
+     * @dataProvider stubbedPathsProvider
      */
-    public function it_displays_the_local_page_stub_instead_of_a_page_that_is_hosted_elsewhere()
+    public function it_displays_the_local_page_stub_instead_of_a_page_that_is_hosted_elsewhere($path)
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/about');
+        $crawler = $client->request('GET', $path);
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
         $this->assertSame('Local Page Stub', $crawler->filter('.content-header__title')->text());
+    }
+
+    public function stubbedPathsProvider() : array
+    {
+        return [
+            'stubbed about page' => ['/about'],
+        ];
     }
 
     protected function getUrl() : string
