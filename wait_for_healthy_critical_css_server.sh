156c7700277dcb13a620b144d3eb5130700310b2
David Moulton <d.moulton@elifesciences.org>
2024-10-18 14:43:33 +0100

    Wait for up to 30s for the first page used in critical css generation to be healthy elifesciences/issues#8964

--- wait_for_healthy_critical_css_server.sh
+++ wait_for_healthy_critical_css_server.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -e
 
-echo 'inside wait for healthy critical css server'
+echo 'waiting for /resources to be healthy'
+
+timeout="30"
+
+timeout --foreground "$timeout" bash << EOT
+    while true; do
+        curl ${CRITICAL_CSS_BASE_URL}/resources >/dev/null 2> /dev/null && exit 0
+        sleep 1
+    done
+EOT
+
+echo '/resources is healthy'
