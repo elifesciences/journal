619bfe7125c5594df0485ed39ca97ce1c605fb64
Giorgio Sironi <g.sironi@elifesciences.org>
2024-10-18 14:49:40 +0100

    Extract a named variable to remove duplication elifesciences/issues#8964

--- wait_for_healthy_critical_css_server.sh
+++ wait_for_healthy_critical_css_server.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-echo 'waiting for /resources to be healthy'
+critical_css_healthy_test_url="${CRITICAL_CSS_BASE_URL}/resources"
+echo "waiting for ${critical_css_healthy_test_url} to be healthy"
 
 timeout="30"
 
 timeout --foreground "$timeout" bash << EOT
     while true; do
-        curl ${CRITICAL_CSS_BASE_URL}/resources >/dev/null 2> /dev/null && exit 0
+        curl "${critical_css_healthy_test_url}" >/dev/null 2> /dev/null && exit 0
         sleep 1
     done
 EOT
 
-echo '/resources is healthy'
+echo "${critical_css_healthy_test_url} is healthy"
