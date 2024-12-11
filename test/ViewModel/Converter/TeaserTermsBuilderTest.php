c1374ce8b33e504c0fa412726a5291d81ddcdfe5
LinaKind <l.kinduryte@elifesciences.org>
2024-12-11 11:31:11 +0000

    Write a characterisation test list for teaser terms builder
    https://github.com/elifesciences/issues/issues/9034

---
+++ test/ViewModel/Converter/TeaserTermsBuilderTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace test\eLife\Journal\ViewModel\Converter;
+
+use eLife\Journal\ViewModel\Converter\ReviewedPreprintTeaserConverter;
+use eLife\Journal\ViewModel\Converter\ViewModelConverter;
+use eLife\Patterns\ViewModel;
+use PHPUnit\Framework\TestCase;
+
+final class TeaserTermsBuilderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    final public function it_builds_significance_terms_when_there_are_significance_terms()
+    {
+        $this->markTestIncomplete('incomplete');
+    }
+
+    /**
+     * @test
+     */
+    final public function it_does_not_build_significance_terms_when_none_are_available()
+    {
+        $this->markTestIncomplete('incomplete');
+    }
+
+    /**
+     * @test
+     */
+    final public function it_builds_strength_terms_when_there_are_strength_terms()
+    {
+        $this->markTestIncomplete('incomplete');
+    }
+
+    /**
+     * @test
+     */
+    final public function it_does_not_build_strength_terms_when_none_are_available()
+    {
+        $this->markTestIncomplete('incomplete');
+    }
+
+    /**
+     * @test
+     */
+    final public function it_capitalises_the_first_letter_of_the_term()
+    {
+        $this->markTestIncomplete('incomplete');
+    }
+}
