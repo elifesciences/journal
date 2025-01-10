c4c9057bd870aed2f8d71d326e64cd1f672c7961
LinaKind <l.kinduryte@elifesciences.org>
2025-01-10 12:03:52 +0000

    Update api-sdk for ElifeAssessment to provide ArticleSection
    https://github.com/elifesciences/issues/issues/9034

--- test/ViewModel/Converter/TeaserTermsBuilderTest.php
+++ test/ViewModel/Converter/TeaserTermsBuilderTest.php
@@ -2,7 +2,10 @@
 
 namespace test\eLife\Journal\ViewModel\Converter;
 
+use eLife\ApiSdk\Model\ArticleSection;
 use eLife\ApiSdk\Model\ElifeAssessment;
+use eLife\ApiSdk\Model\Block\Paragraph;
+use eLife\ApiSdk\Collection\ArraySequence;
 use eLife\Journal\ViewModel\Converter\TeaserTermsBuilder;
 use eLife\Patterns\ViewModel\TeaserTerms;
 use eLife\Patterns\ViewModel\Term;
@@ -16,6 +19,7 @@ final class TeaserTermsBuilderTest extends TestCase
     private $builder;
 
     private $elifeAssessmentTitle = 'Lorem ipsum';
+    private $articleSection;
 
      /**
      * @before
@@ -23,6 +27,7 @@ final class TeaserTermsBuilderTest extends TestCase
     public function setUpConverter()
     {
         $this->builder = new TeaserTermsBuilder();
+        $this->articleSection = new ArticleSection(new ArraySequence([new Paragraph('eLife assessment')]));
     }
 
     /**
@@ -30,7 +35,7 @@ final class TeaserTermsBuilderTest extends TestCase
      */
     final public function it_builds_significance_terms_when_there_are_significance_terms_and_no_strength_terms_are_available()
     {
-        $elifeAssessment = new ElifeAssessment($this->elifeAssessmentTitle, ['landmark'], null);
+        $elifeAssessment = new ElifeAssessment($this->elifeAssessmentTitle, $this->articleSection, ['landmark'], null);
         $result = $this->builder->build($elifeAssessment);
 
         $expected = new TeaserTerms([new Term('Landmark')]);
@@ -42,7 +47,7 @@ final class TeaserTermsBuilderTest extends TestCase
      */
     final public function it_does_not_build_significance_terms_when_there_are_none_in_the_assessment_and_no_strength_terms_are_available()
     {
-        $elifeAssessment = new ElifeAssessment($this->elifeAssessmentTitle, [], null);
+        $elifeAssessment = new ElifeAssessment($this->elifeAssessmentTitle, $this->articleSection, [], null);
         $result = $this->builder->build($elifeAssessment);
 
         $this->assertNull($result);
@@ -53,7 +58,7 @@ final class TeaserTermsBuilderTest extends TestCase
      */
     final public function it_builds_strength_terms_when_there_are_strength_terms_and_no_significance_terms_are_available()
     {
-        $elifeAssessment = new ElifeAssessment($this->elifeAssessmentTitle, null, ['convincing']);
+        $elifeAssessment = new ElifeAssessment($this->elifeAssessmentTitle, $this->articleSection, null, ['convincing']);
         $result = $this->builder->build($elifeAssessment);
 
         $expected = new TeaserTerms([new Term('Convincing')]);
@@ -65,7 +70,7 @@ final class TeaserTermsBuilderTest extends TestCase
      */
     final public function it_builds_terms_using_both_significance_and_strength_terms_in_that_order()
     {
-        $elifeAssessment = new ElifeAssessment($this->elifeAssessmentTitle, ['landmark'], ['solid']);
+        $elifeAssessment = new ElifeAssessment($this->elifeAssessmentTitle, $this->articleSection, ['landmark'], ['solid']);
         $result = $this->builder->build($elifeAssessment);
 
         $expected = new TeaserTerms([new Term('Landmark'), new Term('Solid')]);
