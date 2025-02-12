21fbe3e1bbd68869717b57246c29c634683a4f6a
David Moulton <d.moulton@elifesciences.org>
2025-02-12 15:34:08 +0000

    Create ElifeAssessmentTermsFilterTest file with a todo test
    https://github.com/elifesciences/issues/issues/9169

---
+++ test/Controller/ElifeAssessmentTermsFilterTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace test\eLife\Journal\Controller;
+
+use PHPUnit\Framework\TestCase;
+
+final class ElifeAssessmentTermsFilterTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_translates_a_minimum_significance_of_important_to_a_set_of_filters()
+    {
+        $this->markTestIncomplete();
+    }
+}
