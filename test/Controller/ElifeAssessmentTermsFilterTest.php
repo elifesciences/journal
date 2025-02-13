11d809d4139a4df8cf30a5650be860992e7b772a
LinaKind <l.kinduryte@elifesciences.org>
2025-02-13 10:20:24 +0000

    Add another test case for minimum significance terms filter
    https://github.com/elifesciences/issues/issues/9169

--- test/Controller/ElifeAssessmentTermsFilterTest.php
+++ test/Controller/ElifeAssessmentTermsFilterTest.php
@@ -32,4 +32,18 @@ final class ElifeAssessmentTermsFilterTest extends TestCase
         $this->assertContains('useful', $result);
         $this->assertCount(5, $result);
     }
+
+    /**
+     * @test
+     */
+    public function it_translates_empty_minimum_significance_to_a_complete_set_of_filters()
+    {
+        $result = ElifeAssessmentTermsFilter::fromMinimumSignificance('');
+        $this->assertContains('important', $result);
+        $this->assertContains('landmark', $result);
+        $this->assertContains('fundamental', $result);
+        $this->assertContains('valuable', $result);
+        $this->assertContains('useful', $result);
+        $this->assertCount(5, $result);
+    }
 }
