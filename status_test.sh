54d465e90e70e4af8027b23b269a4581c5679a1f
LinaKind <l.kinduryte@elifesciences.org>
2025-03-03 11:56:36 +0200

    Improve observability by logging curl requests made during smoke tests
    https://github.com/elifesciences/issues/issues/9208

--- status_test.sh
+++ status_test.sh
@@ -4,6 +4,7 @@
 hostname="${1:-$(hostname)}"
 port="${2:-80}"
 
+echo "curl -v --location "$hostname:$port/status" | grep check__name"
 checks=$(curl -v --location "$hostname:$port/status" | grep check__name)
 echo "$checks"
 good_checks=$(echo "$checks" | grep -o ✔ | wc -l)
