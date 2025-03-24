2966465ef41f610e0a711c951d83a8e64c21b957
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2025-03-24 15:01:09 +0000

    Rename to make space to introduce new statistics
    https://github.com/elifesciences/issues/issues/9245

--- src/Controller/Metrics.php
+++ src/Controller/Metrics.php
@@ -12,31 +12,30 @@ class Metrics
     public static function build(
         string $apiEndPoint,
         string $itemId,
-        int $pageViews = null,
-        int $downloads = null,
-        CitationsMetric $citations = null
+        int $totalPageViews = null,
+        int $totalDownloads = null,
+        CitationsMetric $totalCitations = null
     )
     {
-        $statistics = [];
-        $statisticsExtra = [];
-        $statisticsDescription = [];
+        $totalStatistics = [];
+        $barChartsForTotals = [];
 
-        if ($pageViews) {
-            $statistics[] = ViewModel\Statistic::fromNumber('views', $pageViews);
-            $statisticsExtra[] = new ViewModel\BarChart($itemId, 'article', 'page-views', $apiEndPoint, 'page-views', 'month');
+        if ($totalPageViews) {
+            $totalStatistics[] = ViewModel\Statistic::fromNumber('views', $totalPageViews);
+            $barChartsForTotals[] = new ViewModel\BarChart($itemId, 'article', 'page-views', $apiEndPoint, 'page-views', 'month');
         }
 
-        if ($downloads) {
-            $statistics[] = ViewModel\Statistic::fromNumber('downloads', $downloads);
-            $statisticsExtra[] = new ViewModel\BarChart($itemId, 'article', 'downloads', $apiEndPoint, 'downloads', 'month');
+        if ($totalDownloads) {
+            $totalStatistics[] = ViewModel\Statistic::fromNumber('downloads', $totalDownloads);
+            $barChartsForTotals[] = new ViewModel\BarChart($itemId, 'article', 'downloads', $apiEndPoint, 'downloads', 'month');
         }
 
-        if ($citations) {
-            $statistics[] = ViewModel\Statistic::fromNumber('citations', $citations->getHighest()->getCitations());
+        if ($totalCitations) {
+            $totalStatistics[] = ViewModel\Statistic::fromNumber('citations', $totalCitations->getHighest()->getCitations());
         }
 
-        $statisticsDescription[] = new Paragraph('Views, downloads and citations are aggregated across all versions of this paper published by eLife.');
+        $totalStatisticsDescription = new Paragraph('Views, downloads and citations are aggregated across all versions of this paper published by eLife.');
 
-        return array_merge([new ViewModel\StatisticCollection(...$statistics)], $statisticsDescription, $statisticsExtra);
+        return array_merge([new ViewModel\StatisticCollection(...$totalStatistics)], [$totalStatisticsDescription], $barChartsForTotals);
     }
 };
