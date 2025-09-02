b690b419de9d03531c11451aae33a0ae356ab7d8
Nathan Lisgo <nathan@lisgo.co.uk>
2025-09-02 10:41:10 +0100

    Cover expected behaviour with tests
    https://github.com/elifesciences/enhanced-preprints-issues/issues/1281

--- test/Controller/HomeControllerTest.php
+++ test/Controller/HomeControllerTest.php
@@ -17,6 +17,7 @@ final class HomeControllerTest extends PageTestCase
         $crawler = $this->getUrlWithSubjectsAndCovers();
 
         $this->assertSame(0, $crawler->filter('.site-header-home-wrapper')->count());
+        $this->assertSame(0, $crawler->filter('.site-header--home-page')->count());
         $this->assertSame(0, $crawler->filter('.main--with-new-designs-borders')->count());
         $this->assertSame(0, $crawler->filter('.banner-and-subjects-wrapper')->count());
         $this->assertSame(0, $crawler->filter('[data-home-banner]')->count());
@@ -39,6 +40,8 @@ final class HomeControllerTest extends PageTestCase
         $this->assertSame(0, $crawler->filter('.hero-banner__details')->count());
         $this->assertSame(1, $crawler->filter('.wrapper--subjects')->count());
         $this->assertEquals(6, $crawler->filter('.highlight-item')->count());
+        $this->markTestIncomplete();
+        $this->assertSame(1, $crawler->filter('.site-header--home-page')->count());
     }
 
     /**
